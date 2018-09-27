18888d2410cce7a63440f9303b7ca33b5a204ce2
matt higger <matt.higger@gmail.com>
2018-09-27 10:22:25 -0400

    minor

--- pnl_segment/test/regressor.py
+++ pnl_segment/test/regressor.py
@@ -14,10 +14,11 @@ from pnl_segment.vox_regress.polyregressor import PolyRegressor
 n_sbj = 1e9
 x_label = ['age', 'sex', 'wrat']
 y_label = ['FA', 'MD']
+degree = 1
 
 # get output data folder
 folder_data = pathlib.Path(__file__).parent / 'data'
-
+folder_pdf = pathlib.Path(__file__).parent / 'pdf'
 # get data paths
 folder = pnl_data.folder_data / 'intrust' / 'fa_md_in_015_NAA_001'
 fa_dict = {get_sbj(f.stem): f for f in folder.glob('*FA.nii.gz')}
@@ -42,18 +43,28 @@ while len(sbj_img_tree) > n_sbj:
 
 # build regressor
 r = PolyRegressor(sbj_img_tree=sbj_img_tree, y_label=y_label, x_label=x_label,
-                  sbj_mask_dict=sbj_mask_dict, degree=2)
-# r.fit(verbose=True, obs_to_var_thresh=5)
+                  sbj_mask_dict=sbj_mask_dict, degree=degree)
+r.fit(verbose=True, obs_to_var_thresh=3)
 
 # save
 f_out = folder_data / 'poly_regress.p.gz'
-# file.save(r, f_out)
-r = file.load(f_out)
+file.save(r, f_out)
+# r = file.load(f_out)
+
+f_ex_reg = folder_pdf / 'poly_regress_ex.pdf'
+with PdfPages(f_ex_reg) as pdf:
+    ijk = next(iter(r.ijk_regress_dict.keys()))
+    r.plot(ijk, x_feat='age', y_feat='FA')
+    plt.suptitle(f'voxel: {ijk}, r2: {r.r2_score[ijk]}')
+    fig = plt.gcf()
+    fig.set_size_inches(8, 8)
+    pdf.savefig(fig)
+    plt.close()
 
 f_r2_image = folder_data / 'poly_regress_r2.nii.gz'
 r.r2_to_nii(f_r2_image)
 
-f_r2_hist = folder_data / 'poly_regress_r2_hist.pdf'
+f_r2_hist = folder_pdf / 'poly_regress_r2_hist.pdf'
 with PdfPages(f_r2_hist) as pdf:
     r2 = r.r2_score.flatten()
     r2[np.logical_not(np.isfinite(r2))] = -1
