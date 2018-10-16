c046d09322f056b872e30e6b00d3c5c9cc5d16ca
matt higger <matt.higger@gmail.com>
2018-10-16 14:19:19 -0400

    clean - part_graph_factory - needs some more work

--- pnl_segment/adaptive/part_graph_factory.py
+++ pnl_segment/adaptive/part_graph_factory.py
@@ -11,13 +11,13 @@ import nibabel as nib
 import numpy as np
 from tqdm import tqdm
 
-from . import region, part_graph, feat_stat
-from ..point_cloud import point_cloud_ijk, ref_space
+from pnl_segment.adaptive import region, part_graph, feat_stat
+from pnl_segment.point_cloud import point_cloud_ijk, ref_space
+from pnl_segment.simulate.mask import Mask
 
 
 def part_graph_factory(obj, f_img_dict, verbose=False, f_mask=None,
-                       f_edge_constraint=None, history=False, sbj_thresh=.95,
-                       img_label=None):
+                       f_edge_constraint=None, history=False, thresh_mask=.95):
     """ init PartGraph via img
 
     Args:
@@ -29,9 +29,15 @@ def part_graph_factory(obj, f_img_dict, verbose=False, f_mask=None,
                                     ['FA_sbj2.nii.gz', 'MD_sbj2.nii.gz']],
                             'healthy': ... }
                             all should be in same space
-
         verbose (bool): toggles command line output
-        sbj_thresh (float): percentage of sbj needed to include ijk voxel
+        f_mask (str or None): masks part_graph, only operates within mask, if
+                              str reads in mask from nii.  if None, built as
+                              intersection of nonzero values in f_img_dict
+        f_edge_constraint (str): path to segmentation nii, no voxels from
+                                 separate regions may be combined
+        history (bool): toggles whether part_graph keeps history (see
+                        PartGraphHistory)
+        thresh_mask (float): percentage of values needed to include ijk voxel
 
     Returns:
         part_graph (PartGraph)
@@ -49,29 +55,20 @@ def part_graph_factory(obj, f_img_dict, verbose=False, f_mask=None,
     if verbose:
         print('construct part_graph: load')
 
-    # load mask
-    if f_mask is None:
-        mask = None
-    else:
-        img_mask = nib.load(str(f_mask))
-        mask = img_mask.get_data() > 0
-
     # init empty graph
     if history:
         pg = part_graph.PartGraphHistory()
     else:
         pg = part_graph.PartGraph()
-    pg.feat_label = img_label
 
     # get ijk_dict_tree, (ijk_dict_tree[grp][ijk_tuple] = feat_stat)
     ijk_dict_tree = dict()
     for grp, f_list_list in f_img_dict.items():
         if verbose:
             print(f'----{grp}----')
-        min_sbj = np.floor(sbj_thresh * len(f_list_list)).astype(int)
         ijk_dict_tree[grp] = get_ijk_dict(f_list_list, verbose=verbose,
-                                          min_sbj=min_sbj, mask=mask,
-                                          feat_label=pg.feat_label)
+                                          thresh_mask=thresh_mask,
+                                          f_mask=f_mask)
 
     # get set of all ijk
     ijk_set = set()
@@ -87,7 +84,7 @@ def part_graph_factory(obj, f_img_dict, verbose=False, f_mask=None,
             raise AttributeError(f'shape / affine mismatch: {f}, {f_list[0]}')
 
     if verbose:
-        print('\n' * 2 + 'construct part_graph: build graph')
+        print('construct part_graph: build graph')
 
     # build graph_nodes
     tqdm_dict = {'desc': 'build nodes (per ijk)',
@@ -117,7 +114,7 @@ def part_graph_factory(obj, f_img_dict, verbose=False, f_mask=None,
               verbose=verbose)
 
     if verbose:
-        print('finished constructing part_graph' + '\n' * 2)
+        print('finished constructing part_graph')
 
     # store original image files
     pg.f_img_dict = f_img_dict
@@ -157,8 +154,8 @@ def add_edges(pg, reg_by_ijk, verbose=False, f_edge_constraint=None):
             pg.add_edge(reg1, reg2)
 
 
-def get_ijk_dict(f_img_list_iter, verbose=False, min_sbj=1, mask=None,
-                 feat_label=None, raw_feat=False):
+def get_ijk_dict(f_img_list_iter, verbose=False, thresh_mask=.95, f_mask=None,
+                 raw_feat=False):
     """ reads in images, builds feat_stat per ijk location
 
     Args:
@@ -167,9 +164,9 @@ def get_ijk_dict(f_img_list_iter, verbose=False, min_sbj=1, mask=None,
                              [['FA_sbj1.nii.gz', 'MD_sbj1.nii.gz'], \
                               ['FA_sbj2.nii.gz', 'MD_sbj2.nii.gz']],
         verbose (bool): toggles cmd line output
-        min_sbj (int): min num sbj to include ijk
-        mask (array): boolean array, include only ijk entries which are True
-        feat_label (iter): labels each dimension (e.g. ('fa', 'md'))
+        f_mask (str): path to nii of mask
+        thresh_mask (int): % of images with positive value required to include
+                           voxel ijk (only used if f_mask = None)
         raw_feat (bool): toggles getting raw features
     Returns:
         feat_dict (dict): keys are ijk (tuple), values are FeatStat
@@ -189,29 +186,24 @@ def get_ijk_dict(f_img_list_iter, verbose=False, min_sbj=1, mask=None,
     # feat is 5d array with shape (space, space, space, num_feat, num_sbj)
     feat = np.stack(feat_list, axis=4)
 
-    # check mask
-    if mask is not None and not np.allclose(feat.shape[:3], mask.shape):
-        raise AttributeError('mask not same size as feat')
+    # build mask
+    if f_mask is not None:
+        mask = Mask.from_nii(f_mask)
+    else:
+        f_nii_list = [f for l in f_img_list_iter for f in l]
+        mask = Mask.build_intersection_from_nii(f_nii_list, thresh=thresh_mask)
 
     # compute feat stats
     dict_out = dict()
     tqdm_dict = {'total': np.prod(feat.shape[:3]),
                  'disable': not verbose,
                  'desc': 'compute feat_stat per voxel'}
-    for ijk in tqdm(np.ndindex(feat.shape[:3]), **tqdm_dict):
-        # if mask[ijk] is False, skip this value
-        if mask is not None and not mask[tuple(ijk)]:
-            continue
-
+    for ijk in tqdm(mask, **tqdm_dict):
         # get x, with dim num_feat x num_sbj
         x = feat[ijk[0], ijk[1], ijk[2], :, :]
 
         # remove col which are not both non-zero
         x = x[:, x.all(axis=0)]
-
-        if x.shape[1] < min_sbj:
-            # not enough valid sbj
-            continue
 
         # add it to dict_out
         if raw_feat:
@@ -219,7 +211,6 @@ def get_ijk_dict(f_img_list_iter, verbose=False, min_sbj=1, mask=None,
         else:
             obs_greater_dim = x.shape[1] > x.shape[0]
             fs = feat_stat.FeatStat.from_array(x, obs_greater_dim)
-            fs.label = feat_label
             dict_out[ijk] = fs
 
     return dict_out
