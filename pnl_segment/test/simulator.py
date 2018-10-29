0a1b7723428e3e9e4bf4b925310b695b4ec91a2e
matt higger <matt.higger@gmail.com>
2018-10-29 10:51:35 -0400

    revise - simulate.Simulator - behavior for managing data now in file_tree

--- pnl_segment/test/simulator.py
+++ pnl_segment/test/simulator.py
@@ -1,101 +1,65 @@
+import random
 from collections import defaultdict
 from datetime import datetime
 
-import nibabel as nib
 import numpy as np
-from scipy.ndimage import binary_dilation
-from tqdm import tqdm
 
-from mh_pytools import file, parallel
-from pnl_data.set.cidar_post import folder, get_name
-from pnl_segment.simulate import simulator, mask, effect
-import random
+from mh_pytools import file
+from pnl_data.set.cidar_post import folder as folder_data
+from pnl_data.set.cidar_post import get_name
+from pnl_segment.adaptive.data import FileTree
+from pnl_segment.simulate import simulator
 
-n_effect = 20
-effect_snr_iter = np.logspace(-3, 2, n_effect)
-eff_ratio = .5
+rep_per_snr = 10
+snr_vec = np.logspace(-2, 1, 20)
+p_effect = .5
 effect_rad = 3
-mask_rad = 5
-obj = 'max_maha'
+active_rad = 3
+obj = 'max_kl'
 feat_list = ['fa', 'md']
-# prev_folder_out = folder / '2018_Oct_19_01_57PM42'
-prev_folder_out = None
+f_rba = folder_data / 'fs' / '01193' / 'aparc.a2009s+aseg_in_dti.nii.gz'
+folder = folder_data / '2018_Oct_29_10_32AM20'
+# folder = None
 
 np.random.seed(1)
 random.seed(1)
 
-folder_data = folder / 'dti_in_01193'
-if prev_folder_out is None:
-    folder_out = folder / datetime.now().strftime("%Y_%b_%d_%I_%M%p%S")
-    folder_out.mkdir(exist_ok=True, parents=True)
+if folder is None:
+    # make output folder (timestamped)
+    folder = folder_data / datetime.now().strftime("%Y_%b_%d_%I_%M%p%S")
+    folder.mkdir(exist_ok=True, parents=True)
 
-    # build f_img_health
-    f_img_health = defaultdict(dict)
+    # build sbj_feat_file_tree
+    _folder_data = folder_data / 'dti_in_01193'
+    sbj_feat_file_tree = defaultdict(dict)
     for feat in feat_list:
-        for f in folder_data.glob(f'*{feat}.nii.gz'):
-            f_img_health[get_name(f.stem)][feat] = f
+        for f in _folder_data.glob(f'*{feat}.nii.gz'):
+            sbj_feat_file_tree[get_name(f.stem)][feat] = f
+
+    # only test two sbj
+    for sbj in list(sbj_feat_file_tree.keys())[2:]:
+        del sbj_feat_file_tree[sbj]
 
     # init simulator, split into groups
-    sim = simulator.Simulator(f_img_health=f_img_health)
-    sbj_effect, sbj_health = sim.split_sbj(eff_ratio=.5)
-    file.save((sim, sbj_effect, sbj_health, obj),
-              file=folder_out / 'sim_split.p.gz')
+    file_tree = FileTree(sbj_feat_file_tree=sbj_feat_file_tree, verbose=True)
+    sim = simulator.Simulator(file_tree=file_tree, folder=folder)
+    sim.split(p_effect=p_effect)
 
-    # single healthy (serial) run
-    folder_healthy = folder_out / '_no_effect'
-    sim.run_healthy(obj=obj, save=True, verbose=True, folder=folder_healthy)
+    file.save(sim, folder / 'sim.p.gz')
 else:
-    # allows us to add to previous experiments
-    folder_out = prev_folder_out
-    sim, sbj_effect, sbj_health, obj = file.load(folder_out / 'sim_split.p.gz')
+    sim = file.load(folder / 'sim.p.gz')
 
-# build intersection of all fa as mask of 'prior' location of effect
-f_list_all = [sim.f_img_health[sbj][feat] for feat in feat_list for sbj in sim.f_img_health.keys()]
-mask_all = mask.Mask.build_intersection_from_nii(f_list_all)
+sim.run_effect(snr=1, obj=obj, active_rad=active_rad, radius=effect_rad,
+               f_rba=f_rba, verbose=True)
 
-# load all data (to compute effects via snr we need raw data)
-f_img_tree_data = defaultdict(dict)
-for sbj, d in tqdm(sim.f_img_health.items(), desc='load per sbj'):
-    for feat, f in d.items():
-        f_img_tree_data[sbj][feat] = nib.load(str(f)).get_data()
-
-
-# build effect
-def get_effect(snr):
-    """ generates effect at random location with given snr to healthy
-    """
-    effect_mask = effect.Effect.sample_mask(prior_array=mask_all.x,
-                                            radius=effect_rad)
-    eff = effect.Effect.from_data(f_img_tree=f_img_tree_data,
-                                  mask=effect_mask,
-                                  snr=snr)
-
-    # build mask around affected area and neighboring voxels (faster compute)
-    mask_active = binary_dilation(eff.mask.x, iterations=mask_rad)
-    mask_active = mask.Mask(mask_active,
-                            ref_space=eff.mask.ref_space)
-    f_mask_active = mask_active.to_nii(f_ref=f_list_all[0])
-
-    return eff, f_mask_active
-
-
-# build list of args into run_effect, each its own effect location / snr
-arg_list = list()
-z = np.ceil(np.log10(n_effect)).astype(int)
-for idx, snr in tqdm(enumerate(effect_snr_iter), desc='sample effects'):
-    eff, f_mask_active = get_effect(snr)
-    d = {'obj': obj,
-         'save': True,
-         'effect': eff,
-         'sbj_effect': sbj_effect,
-         'sbj_health': sbj_health,
-         'f_mask': f_mask_active,
-         'folder': folder_out / f'{str(idx).zfill(z)}_snr_{snr:.2E}'}
-    arg_list.append(d)
-
-# serial test
-# for d in arg_list:
-#     sim.run_effect(**d, verbose=True)
-
-# run parallel
-parallel.run_par_fnc(fnc='run_effect', obj=sim, arg_list=arg_list)
+# # sim.run_healthy(obj=obj)
+#
+# arg_list = list()
+# for snr in snr_vec:
+#     for _ in range(rep_per_snr):
+#         arg_list.append({'snr': snr,
+#                          'obj': obj,
+#                          'active_rad': active_rad,
+#                          'radius': effect_rad})
+#
+# parallel.run_par_fnc('run_effect', arg_list=arg_list, obj=sim)
