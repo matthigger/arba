4d02cf2b8e947c61c5a6034c908442890ab29d1d
matt higger <matt.higger@gmail.com>
2018-11-09 12:35:11 -0500

    fix - region.__init__ - absolute to relative

--- pnl_segment/region/__init__.py
+++ pnl_segment/region/__init__.py
@@ -1,7 +1,7 @@
-from pnl_segment.region.feat_stat import *
-from pnl_segment.region.reg_kl import *
-from pnl_segment.region.reg_maha import *
-from pnl_segment.region.reg_min_var import *
-from pnl_segment.region.reg import *
+from .feat_stat import *
+from .reg_kl import *
+from .reg_maha import *
+from .reg_min_var import *
+from .reg import *
 
 __all__ = ['feat_stat', 'reg_kl', 'reg_maha', 'reg_min_var', 'reg.py']
