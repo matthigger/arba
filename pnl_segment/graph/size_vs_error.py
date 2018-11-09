6f1d77d2dd6081fd30bc7b83b25da9d53f5e4bfe
matt higger <matt.higger@gmail.com>
2018-11-09 09:47:53 -0500

    add - graph.size_vs_error

---
+++ pnl_segment/graph/size_vs_error.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def size_vs_error(pg_hist):
+    size = list()
+    error = list()
+
+    for pg in pg_hist:
+        size.append(len(pg))
+        error.append(sum(r.error for r in pg.nodes))
+
+    sns.set()
+    plt.plot(size, error, label='error')
+
+    max_err = max(error)
+    max_size = max(size)
+    error_reg = [e + (s - 1) * max_err / (max_size - 1) for e, s in
+                 zip(error, size)]
+    min_reg_size = min(zip(error_reg, size))[1]
+
+    plt.plot(size, error_reg, label='regularized error')
+    plt.axvline(min_reg_size, label=f'optimal size: {min_reg_size}', color='r')
+    plt.legend()
+
+    plt.xlabel('size (num regions in segmentation)')
+    plt.ylabel('error in cluster (var added from voxel to region)')
