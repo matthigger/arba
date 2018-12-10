ab16ee827b3a389e0f567ba80066e3561a4c782d
matt higger <matt.higger@gmail.com>
2018-12-10 10:57:05 -0500

    rm - test

--- pnl_segment/test/space/mask.py
+++
@@ -1,8 +0,0 @@
-from pnl_segment.space import *
-import pathlib
-
-f_mask_nii = pathlib.Path(__file__).parents[1] / 'data' / 'mask.nii.gz'
-
-m = Mask.from_nii(f_mask_nii)
-len(m)
-pc = PointCloud.from_mask(m)
