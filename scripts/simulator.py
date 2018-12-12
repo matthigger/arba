a19364ae9afc0d2e3d765e6de5b77d1c9b7b9f70
matt higger <matt.higger@gmail.com>
2018-12-12 08:04:21 -0500

    minor

--- scripts/simulator.py
+++ scripts/simulator.py
@@ -10,7 +10,7 @@ from pnl_data.set.cidar_post import get_name
 from pnl_segment.seg_graph.data import FileTree
 from pnl_segment.simulate import simulator
 
-num_locations = 10
+num_locations = 4
 snr_vec = np.logspace(-1, 1, 5)
 # snr_vec = [.2]
 p_effect = .5
@@ -19,11 +19,11 @@ active_rad = 5
 obj = 'maha'
 feat_list = ['fa', 'md']
 effect_u = np.array([-1, 0])
-resample = True
+resample = False
 harmonize = True
 par_flag = False
 f_rba = folder_data / 'fs' / '01193' / 'aparc.a2009s+aseg_in_dti.nii.gz'
-folder = folder_data / '2018_Nov_16_12_34AM35'
+folder = folder_data / '2018_Dec_11_03_30PM19'
 # folder = None
 
 np.random.seed(1)
@@ -40,10 +40,6 @@ if folder is None:
     for feat in feat_list:
         for f in _folder_data.glob(f'*{feat}.nii.gz'):
             sbj_feat_file_tree[get_name(f.stem)][feat] = f
-
-    # # only test 4 sbj
-    # for sbj in list(sbj_feat_file_tree.keys())[4:]:
-    #     del sbj_feat_file_tree[sbj]
 
     # init simulator, split into groups
     file_tree = FileTree(sbj_feat_file_tree=sbj_feat_file_tree)
@@ -80,4 +76,11 @@ for snr in snr_vec:
             sim.run_effect(verbose=True, **d)
 
 if par_flag:
-    parallel.run_par_fnc('run_effect', arg_list=arg_list, obj=sim)
+    raise NotImplementedError
+    def fnc(**kwargs):
+        sim = file.load(folder / 'sim.p.gz')
+        sim.folder = folder
+        sim.run_effect(**kwargs)
+
+
+    parallel.run_par_fnc(fnc, arg_list=arg_list)
