0d5fe9cab415974b4d571af5698fc703ffa336b3
matt higger <matt.higger@gmail.com>
2018-12-28 11:53:23 -0500

    revise - plot.report

--- pnl_segment/plot/report.py
+++ pnl_segment/plot/report.py
@@ -1,65 +1,114 @@
+from collections import defaultdict
+
+import matplotlib.gridspec as gridspec
+import nibabel as nib
 import numpy as np
 import seaborn as sns
-from graph.region import plot_stat, roi_and_prob_feat
-from graph.scatter_tree import size_v_mahalanobis
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from nilearn.plotting import plot_roi
+from tqdm import tqdm
 
-from pnl_segment.seg_graph.factory import get_ijk_dict
-
-
-def fnc_sort(reg, grp_cmp='healthy', grp_test='effect'):
-    raise AttributeError('reimplement pval')
-    # p, r2 = pval.get_pval(reg, grp_cmp=grp_cmp, grp_test=grp_test)
-
-    return -r2, p
+from pnl_segment.space import PointCloud, get_ref
 
 
-def plot_report(pg, f_out, f_mask_effect=None, f_back=None):
+def plot_reg(reg, f_back, f_mask=None, ax=None):
+    if f_mask is not None:
+        raise NotImplementedError
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+
+    ref = get_ref(f_back)
+    roi = PointCloud(reg.pc_ijk).to_mask(shape=ref.shape).astype(int)
+    roi_img = nib.Nifti1Image(roi, affine=ref.affine)
+
+    cm = plt.cm.get_cmap('viridis')
+    plot_roi(roi_img=roi_img, bg_img=str(f_back), axes=ax, cmap=cm,
+             draw_cross=False)
+
+
+def plot_feat(reg, ft_dict, feat_x, feat_y, grp_data_dict=None, ax=None,
+              verbose=False):
+    # get idx of features
+    ft = next(iter(ft_dict.values()))
+    idx_feat_x = ft.feat_list.index(feat_x)
+    idx_feat_y = ft.feat_list.index(feat_y)
+
+    if grp_data_dict is None:
+        grp_data_dict = {grp: ft.get_array(verbose=verbose) for grp, ft in
+                         ft_dict.items()}
+
+    grp_feat_data_dict = defaultdict(lambda: defaultdict(list))
+    for grp, data in grp_data_dict.items():
+        for ijk in reg.pc_ijk:
+            x = data[ijk[0], ijk[1], ijk[2], :, :]
+            grp_feat_data_dict[grp][feat_x].append(np.mean(x[idx_feat_x, :]))
+            grp_feat_data_dict[grp][feat_y].append(np.mean(x[idx_feat_y, :]))
+
+    # build new axis if need be
+    if ax is None:
+        fig, ax = plt.subplots(1, 1)
+
+    # plot
+    sns.set(font_scale=1.2)
+    grp_list = sorted(grp_data_dict.keys())
+    cm = plt.cm.get_cmap('Set1')
+    grp_color_dict = {grp: cm(idx) for idx, grp in enumerate(grp_list)}
+    for grp, feat_data_dict in grp_feat_data_dict.items():
+        x = feat_data_dict[feat_x]
+        y = feat_data_dict[feat_y]
+        if len(x) > 1000:
+            x = np.random.choice(x, size=1000, replace=False)
+            y = np.random.choice(y, size=1000, replace=False)
+        ax.scatter(x, y, color=grp_color_dict[grp], label=grp, alpha=.5)
+
+    for grp, feat_data_dict in grp_feat_data_dict.items():
+        x = feat_data_dict[feat_x]
+        y = feat_data_dict[feat_y]
+        ax.scatter(np.mean(x), np.mean(y), marker='s', alpha=1,
+                   color=grp_color_dict[grp], edgecolors='k')
+
+    plt.legend()
+    plt.xlabel(feat_x)
+    plt.ylabel(feat_y)
+
+
+# https://stackoverflow.com/questions/9647202/ordinal-numbers-replacement
+def ordinal(n):
+    return "%d%s" % (
+        n, "tsnrhtdd"[(n / 10 % 10 != 1) * (n % 10 < 4) * n % 10::4])
+
+
+def plot_report(reg_list, ft_dict, f_out, feat_x, feat_y, f_mask=None,
+                f_back=None, verbose=True, ):
+    # load data
+    grp_data_dict = {grp: ft.get_array(verbose=verbose)
+                     for grp, ft in ft_dict.items()}
+
+    tqdm_dict = {'disable': not verbose,
+                 'desc': 'print graph per region'}
+
     sns.set(font_scale=1.2)
     with PdfPages(f_out) as pdf:
-        size_v_mahalanobis(pg.tree_history, f_mask_track=f_mask_effect,
-                           mask_label='% effect')
-        fig = plt.gcf()
-        fig.set_size_inches(12, 8)
-        pdf.savefig(fig)
-        plt.close()
+        # sort by pval
+        reg_list = sorted(reg_list, key=lambda r: r.pval)
+        for idx, reg in tqdm(enumerate(reg_list), **tqdm_dict):
+            fig = plt.figure()
 
-        pg_span = pg.get_min_spanning_region(fnc=fnc_sort)
-        size_v_mahalanobis(pg_span, f_mask_track=f_mask_effect,
-                           mask_label='% effect', edge=False)
-        fig = plt.gcf()
-        fig.set_size_inches(12, 8)
-        pdf.savefig(fig)
-        plt.close()
+            gs = gridspec.GridSpec(1, 2, width_ratios=[3, 1])
 
-        # plot
-        reg_dict = {reg: -np.log10(fnc_sort(reg)[1]) for reg in pg_span}
-        plot_stat(reg_dict, f_back=f_back, label='-log10 pval')
-        fig = plt.gcf()
-        fig.set_size_inches(14, 5)
-        pdf.savefig(fig)
-        plt.close()
+            ax = plt.subplot(gs[0]), plt.subplot(gs[1])
 
-        # load original data (allows scatter)
-        raw_feat = {grp: get_ijk_dict(img_iter, raw_feat=True) for
-                    grp, img_iter in
-                    pg.f_img_dict.items()}
+            plot_feat(reg, ft_dict, ax=ax[1], feat_x=feat_x, feat_y=feat_y,
+                      grp_data_dict=grp_data_dict, verbose=verbose)
+            plot_reg(reg, f_back, f_mask, ax=ax[0])
 
-        # sort by mahalanobis
-        reg_history_sort = sorted(pg_span.nodes, key=fnc_sort)
-        for idx, reg in enumerate(reg_history_sort[:40]):
-            roi_and_prob_feat(reg, f_back=f_back, f_mask=f_mask_effect,
-                              raw_feat=raw_feat, xlim=(0, 1.3), ylim=(-.5, .5))
-
-            fig = plt.gcf()
             fig.set_size_inches(14, 5)
-            neg_r2, p = fnc_sort(reg)
-            s = len(reg.pc_ijk)
-            label = f'{idx + 1} Abnormal Region ' + \
-                    f'(p-value: {p:.1e}, ' \
-                    f'mah_dist: {-neg_r2:.1f}, ' + \
-                    f'size: {s:.0f} voxels)'
+            label = f'{ordinal(idx + 1)} Abnormal Region: ' + \
+                    f'pval: {reg.pval:.3e}, ' \
+                        f'size: {len(reg):.0f} vox'
             plt.suptitle(label)
+
             pdf.savefig(fig)
             plt.close()
