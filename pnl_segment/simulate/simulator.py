1cd5c6cf47eeddfbf97c4fa69a7a5c0a857fe99e
matt higger <matt.higger@gmail.com>
2019-01-10 10:58:21 -0500

    revise - simulate.simulator - now encapsulates running in parallel and splitting etc

--- pnl_segment/simulate/simulator.py
+++ pnl_segment/simulate/simulator.py
@@ -1,9 +1,13 @@
 import pathlib
+import random
 
 import numpy as np
+from tqdm import tqdm
 
-from .effect import Effect
-from ..seg_graph import FeatStatEmpty, FileTree, run_arba
+from mh_pytools import parallel
+from . import Effect
+import pathlib
+from ..seg_graph import run_arba
 from ..space import PointCloud
 
 
@@ -19,65 +23,88 @@ def increment_to_unique(folder, num_width=3):
 
 
 class Simulator:
-    """ manages simulation: paths, samples effects, runs seg_graph.reduce()
-
-    Attributes:
-        file_tree (FileTree):
-    """
     grp_effect = 'grp_effect'
     grp_null = 'grp_null'
 
-    def __init__(self, file_tree, folder, eff_prior_arr=None, p_effect=.5):
-        self.ref = file_tree.ref
-        self.pc = PointCloud.from_mask(file_tree.mask)
-        self.feat_list = file_tree.feat_list
-        self.folder = folder
-
-        self.eff_prior_arr = eff_prior_arr
-        if self.eff_prior_arr is None:
-            self.eff_prior_arr = file_tree.mask
+    def __init__(self, folder, file_tree, p_effect=.5):
+        self.folder = pathlib.Path(folder)
+        folder.mkdir(parents=True)
 
         # split into two file_trees
+        self.file_tree = file_tree
         ft_eff, ft_null = file_tree.split(p=p_effect)
         self.ft_dict = {self.grp_effect: ft_eff,
                         self.grp_null: ft_null}
 
-    def run_effect(self, maha, active_rad=None, effect_mask=None,
-                   harmonize=True, **kwargs):
+        self.effect_list = list()
+
+    def build_effect_list(self, n_effect, effect_rad, verbose=False):
+        # load
+        self.file_tree.load(verbose=True)
+
+        # build effects (such that their locations are constant across maha)
+        self.effect_list = list()
+        tqdm_dict = {'desc': 'sample effect mask',
+                     'total': n_effect,
+                     'disable': not verbose}
+        for _ in tqdm(range(n_effect), **tqdm_dict):
+            mask = Effect.sample_mask(prior_array=self.file_tree.mask,
+                                      radius=effect_rad,
+                                      ref=self.file_tree.ref)
+
+            # compute feat_stat in mask
+            pc = PointCloud.from_mask(mask)
+            fs = sum(self.file_tree.ijk_fs_dict[ijk] for ijk in pc)
+
+            # maha below is placeholder, it is reset with each run
+            e = Effect.from_fs_maha(fs=fs, mask=mask, maha=1)
+            self.effect_list.append(e)
+
+        # delete voxel wise stats
+        self.file_tree.unload()
+
+    def run_effect(self, effect, maha, active_rad=None, **kwargs):
         # get folder
         folder = increment_to_unique(self.folder / f'maha{maha:.3E}')
 
-        # sample effect
-        if effect_mask is None:
-            effect_mask = Effect.sample_mask(prior_array=self.eff_prior_arr)
-
         # get mask of active area
-        mask_active = self.pc.to_mask()
+        mask_active = self.file_tree.mask
         if active_rad is not None:
             # only work in a dilated region around the effect
-            mask_eff_dilated = effect_mask.dilate(active_rad)
+            mask_eff_dilated = effect.mask.dilate(active_rad)
             mask_active = np.logical_and(mask_eff_dilated, mask_active)
 
-        # (ft_dict has no memory intersection with self.ft_dict)
-        ft_dict = dict()
-        for grp, ft in self.ft_dict.items():
-            ft_dict[grp] = FileTree(sbj_feat_file_tree=ft.sbj_feat_file_tree)
-            ft_dict[grp].load(mask=mask_active, **kwargs)
-
-        # compute stats of all observed data within mask
-        effect_pc = PointCloud.from_mask(effect_mask)
-        fs = FeatStatEmpty()
-        for ft in ft_dict.values():
-            for ijk in effect_pc:
-                fs += ft.ijk_fs_dict[ijk]
-
-        # build effect
-        effect = Effect.from_data(fs=fs, maha=maha, mask=effect_mask, **kwargs)
+        # set scale of effect
+        effect.maha = maha
 
         # run effect
-        run_arba(ft_dict=ft_dict,
-                 mask=mask_active,
+        run_arba(mask=mask_active,
                  effect=effect,
                  grp_effect=self.grp_effect,
                  folder_save=folder,
-                 harmonize=harmonize)
+                 ft_dict=self.ft_dict,
+                 **kwargs)
+
+    def run(self, maha_list, par_flag=False, seed=1, **kwargs):
+
+        # reset seed
+        if seed is not None:
+            np.random.seed(seed)
+            random.seed(seed)
+
+        # build arg_list
+        arg_list = list()
+        for maha in maha_list:
+            for effect in self.effect_list:
+                d = {'effect': effect,
+                     'maha': maha,
+                     'verbose': not par_flag}
+                d.update(kwargs)
+                arg_list.append(d)
+
+        # run
+        if par_flag:
+            parallel.run_par_fnc('run_effect', arg_list=arg_list, obj=self)
+        if not par_flag:
+            for d in arg_list:
+                self.run_effect(**d)
--- scripts/simulate_cidar/0_run_arba.py
+++ scripts/simulate_cidar/0_run_arba.py
@@ -1,29 +1,24 @@
-import random
 from collections import defaultdict
 from datetime import datetime
 
 import numpy as np
-from tqdm import tqdm
 
-from mh_pytools import file, parallel
+from mh_pytools import file
 from pnl_data.set.cidar_post import get_name, folder
 from pnl_segment.seg_graph.data import FileTree
-from pnl_segment.simulate import simulator, Effect
+from pnl_segment.simulate import simulator
 
-num_locations = 10
-# maha_vec = np.logspace(-1, 1, 9)
-maha_vec = [0]
-p_effect = .5
+num_locations = 100
+maha_list = np.logspace(-1, 1, 9)
+# maha_list = [0]
 effect_rad = 5
 active_rad = 5
 feat_list = ['fa', 'md']
-effect_u = np.array([-1, 0])
 harmonize = True
-par_flag = False
+par_flag = True
 
 # make output folder (timestamped)
 folder_out = folder / datetime.now().strftime("%Y_%b_%d_%H_%M_%S")
-folder_out.mkdir(exist_ok=True, parents=True)
 
 # build sbj_feat_file_tree
 _folder_data = folder / 'dti_in_01193'
@@ -34,36 +29,11 @@ for feat in feat_list:
 
 # init simulator, split into groups
 file_tree = FileTree(sbj_feat_file_tree=sbj_feat_file_tree)
-sim = simulator.Simulator(file_tree=file_tree, folder=folder_out,
-                          p_effect=p_effect)
+sim = simulator.Simulator(file_tree=file_tree, folder=folder_out)
+sim.build_effect_list(n_effect=num_locations, effect_rad=effect_rad,
+                      verbose=True)
 
 file.save(sim, folder_out / 'sim.p.gz')
 
-np.random.seed(1)
-random.seed(1)
-
-# sample effect locations (constant across maha)
-effect_mask_list = list()
-for _ in tqdm(range(num_locations), desc='sample effect mask'):
-    mask = Effect.sample_mask(prior_array=file_tree.mask,
-                              radius=effect_rad,
-                              ref=file_tree.ref)
-    effect_mask_list.append(mask)
-
-# build arg_list
-arg_list = list()
-for maha in maha_vec:
-    for effect_mask in effect_mask_list:
-        d = {'maha': maha,
-             'active_rad': active_rad,
-             'effect_mask': effect_mask,
-             'harmonize': harmonize,
-             'u': effect_u,
-             'verbose': not par_flag}
-        arg_list.append(d)
-
-if par_flag:
-    parallel.run_par_fnc('run_effect', arg_list=arg_list, obj=sim)
-if not par_flag:
-    for d in arg_list:
-        sim.run_effect(**d)
+sim.run(maha_list, par_flag=par_flag, active_rad=active_rad,
+        harmonize=harmonize)
