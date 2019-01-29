aae0beb140daba3c99e0750087613b3429c3909a
matt higger <matt.higger@gmail.com>
2019-01-29 15:26:09 -0500

    revise - run_tfce

--- scripts/simulate_cidar/1_run_vba_rba_tfce.py
+++ scripts/simulate_cidar/1_run_vba_rba_tfce.py
@@ -6,38 +6,30 @@ import numpy as np
 
 from mh_pytools import file, parallel
 from pnl_data.set.cidar_post import folder
-from pnl_segment.seg_graph import FileTree
 from pnl_segment.simulate import Simulator, compute_tfce
-from pnl_segment.space import Mask
 
 
-def run_vba_rba_tfce(folder, alpha, write_outfile=True, harmonize=False):
+def run_vba_rba_tfce(folder, alpha, write_outfile=True):
     folder_image = folder / 'image'
-    s_mask_sig = 'mask_sig_{method}.nii.gz'
-
-    ft_dict = file.load(folder / 'ft_dict.p.gz')
-    sg_hist_seg = file.load(folder / 'sg_hist_seg.p.gz')
-    mask = Mask.from_nii(folder_image / 'mask.nii.gz')
-    effect = file.load(folder / 'effect.p.gz')
     folder_tfce = folder / 'tfce'
 
-    # build file tree of entire dataset (no folds needed)
-    for ft in ft_dict.values():
-        ft.load(mask=mask)
+    s_mask_sig = 'mask_sig_{method}.nii.gz'
+    ft_dict = file.load(folder / 'ft_dict.p.gz')
+    sg_hist_seg = file.load(folder / 'sg_hist_seg.p.gz')
+    effect = file.load(folder / 'effect.p.gz')
 
-    # harmonize and apply effect
-    if harmonize:
-        FileTree.harmonize_via_add(tuple(ft_dict.values()), apply=True)
-    effect.apply_to_file_tree(ft_dict[Simulator.grp_effect])
+    # build file tree of entire dataset (no folds needed)
+    ft0, ft1 = ft_dict[Simulator.grp_null], ft_dict[Simulator.grp_effect]
 
     # compute tfce
     f_sig = folder_image / s_mask_sig.format(method='tfce')
-    ft_tuple = ft_dict[Simulator.grp_null], ft_dict[Simulator.grp_effect]
-    _, f_sig_list = compute_tfce(ft_tuple, alpha=alpha, mask=mask,
-                                      folder=folder_tfce)
-    shutil.copy(f_sig_list[0], str(f_sig))
+    _, f_sig_list = compute_tfce((ft0, ft1), alpha=alpha, folder=folder_tfce,
+                                 f_data=folder_tfce / 'maha.nii.gz')
+    shutil.copy(f_sig_list[1], str(f_sig))
 
     # compute vba + rba
+    ft0.load(load_ijk_fs=True, load_data=False)
+    ft1.load(load_ijk_fs=True, load_data=False)
     sg_vba_test, _, _ = next(iter(sg_hist_seg))
     sg_vba = sg_vba_test.from_file_tree_dict(ft_dict)
     sg_dict = {'vba': sg_vba, 'rba': copy.deepcopy(sg_vba)}
@@ -47,7 +39,7 @@ def run_vba_rba_tfce(folder, alpha, write_outfile=True, harmonize=False):
     for method, sg in sg_dict.items():
         sg_sig = sg.get_sig(alpha=alpha)
         sg_sig.to_nii(f_out=folder_image / s_mask_sig.format(method=method),
-                      ref=mask.ref,
+                      ref=ft0.ref,
                       fnc=lambda r: np.uint8(1),
                       background=np.uint8(0))
 
@@ -57,7 +49,7 @@ def run_vba_rba_tfce(folder, alpha, write_outfile=True, harmonize=False):
         f_estimate = folder_image / s_mask_sig.format(method=method)
         estimate = nib.load(str(f_estimate)).get_data()
         method_ss_dict[method] = effect.get_sens_spec(estimate=estimate,
-                                                      mask=mask)
+                                                      mask=ft0.mask)
     if write_outfile:
         f_out = folder / 'detect_stats.txt'
         with open(str(f_out), 'w') as f:
@@ -71,21 +63,19 @@ if __name__ == '__main__':
     from collections import defaultdict
     from tqdm import tqdm
 
-    harmonize = True
     par_flag = True
     alpha = .05
     f_rba = folder / 'fs' / '01193' / 'aparc.a2009s+aseg_in_dti.nii.gz'
-    folder = folder / '2019_Jan_28_12_09_06'
+    folder = folder / '2019_Jan_29_14_58_07'
     f_out = folder / 'performance_stats.p.gz'
 
     # find relevant folders, build inputs to run()
     arg_list = list()
-    for folder in folder.glob('*maha*'):
+    for folder in sorted(folder.glob('*maha*')):
         if not folder.is_dir():
             continue
         arg_list.append({'folder': folder,
-                         'alpha': alpha,
-                         'harmonize': harmonize})
+                         'alpha': alpha})
 
     # run per folder
     fnc = lambda: defaultdict(list)
