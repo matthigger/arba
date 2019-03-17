99a848f7829ddd5538191795c8dfc7e3e2f93520
matt higger <matt.higger@gmail.com>
2019-03-17 12:39:00 -0400

    add - file_tree.scale_equalize() - fnc to normlaize scale

--- arba/data/file_tree.py
+++ arba/data/file_tree.py
@@ -4,12 +4,13 @@ import tempfile
 import nibabel as nib
 import numpy as np
 
+from ..region import FeatStat
 from ..space import get_ref, Mask, PointCloud
 
 
 def check_loaded(fnc):
     def wrapped(self, *args, **kwargs):
-        assert self.f_data is not None, 'FileTree must be loaded to call'
+        assert self.data is not None, 'FileTree must be loaded to call'
         return fnc(*args, **kwargs)
 
     return wrapped
@@ -29,6 +30,9 @@ class FileTree:
         feat_list (list): list of features (defines indexing)
         scale (np.array): defines scaling of data
         ref (RefSpace): defines shape and affine of data
+        fnc_list (list): each is a function which is passed self, may
+                         operate on data as needed before it is write
+                         protected
 
     Attributes available after load()
         mask (Mask): mask of active area
@@ -48,7 +52,7 @@ class FileTree:
     def __len__(self):
         return len(self.sbj_feat_file_tree.keys())
 
-    def __init__(self, sbj_feat_file_tree):
+    def __init__(self, sbj_feat_file_tree, fnc_list=None):
         self.sbj_feat_file_tree = sbj_feat_file_tree
         self.sbj_list = sorted(self.sbj_feat_file_tree.keys())
         feat_file_dict = next(iter(self.sbj_feat_file_tree.values()))
@@ -56,52 +60,47 @@ class FileTree:
         self.scale = np.eye(self.d)
         self.ref = get_ref(next(iter(feat_file_dict.values())))
 
+        self.fnc_list = fnc_list
+        if fnc_list is None:
+            self.fnc_list = list()
+
         self.mask = None
         self.pc = None
         self.fs = None
         self.data = None
         self.f_data = None
 
-    def load(self, fnc_list=None):
-        """ loads data
-
-        Args:
-            fnc_list (list): each is a function which is passed data.  allows
-                             modification of data before it is write protected
-                             (see self.scale_equalize() for example call sig)
+    def __enter__(self):
+        """ loads data, applies fnc in self.fnc_list
         """
         # build memmap file
         shape = (*self.ref.shape, self.num_sbj, self.d)
-        self.f_data = tempfile.NamedTemporaryFile(suffix='.dat').name
-        self.f_data = pathlib.Path(self.f_data)
-        data = np.memmap(self.f_data, dtype='float32', mode='w+', shape=shape)
-        data[:] = 0
+        self.data = np.zeros(shape)
 
         # load data
         for sbj_idx, sbj in enumerate(self.sbj_list):
             for feat_idx, feat in enumerate(self.feat_list):
                 f = self.sbj_feat_file_tree[sbj][feat]
                 img = nib.load(str(f))
-                data[:, :, :, sbj_idx, feat_idx] = img.get_data()
+                self.data[:, :, :, sbj_idx, feat_idx] = img.get_data()
 
         # get pc, mask
-        self.mask = Mask(np.all(data, axis=(3, 4)), ref=self.ref)
+        self.mask = Mask(np.all(self.data, axis=(3, 4)), ref=self.ref)
         self.pc = PointCloud.from_mask(self.mask)
 
         # apply all fnc
-        if fnc_list is not None:
-            for fnc in fnc_list:
-                fnc(data)
+        for fnc in self.fnc_list:
+            fnc(self)
 
         # flush data to memmap, make read only copy
-        del data
+        self.f_data = tempfile.NamedTemporaryFile(suffix='.dat').name
+        self.f_data = pathlib.Path(self.f_data)
+        x = np.memmap(self.f_data, dtype='float32', mode='w+',
+                      shape=shape)
+        x[:] = self.data[:]
+        x.flush()
         self.data = np.memmap(self.f_data, dtype='float32', mode='r',
                               shape=shape)
-
-        return self
-
-    def __enter__(self, *args, **kwargs):
-        self.load(*args, **kwargs)
         return self
 
     def __exit__(self, err_type, value, traceback):
@@ -121,15 +120,6 @@ class FileTree:
         raise NotImplementedError
 
     @check_loaded
-    def scale_equalize(self):
-        """ compute & store mean and var per feat, scale + offset to Z score
-        """
-        # compute features
-
-        # apply scale equalization
-        raise NotImplementedError
-
-    @check_loaded
     def to_nii(self, fnc=None, feat=None, sbj_list=None, f_out=None):
         """ writes data to a nii file
 
@@ -143,3 +133,21 @@ class FileTree:
             f_out (Path): nii file out
         """
         raise NotImplementedError
+
+
+def scale_normalize(ft):
+    """ compute & store mean and var per feat, scale + offset to Z score
+
+    Args:
+        ft (FileTree): file tree to be equalized
+    """
+    # compute stats
+    shape = (len(ft.pc) * ft.num_sbj, ft.d)
+    shape_orig = (len(ft.pc), ft.num_sbj, ft.d)
+    _data = ft.data[ft.mask, :, :].reshape(shape, order='F')
+    ft.fs = FeatStat.from_array(_data.T)
+
+    # apply scale equalization
+    scale = np.diag(1 / np.diag(ft.fs.cov)) ** .5
+    _data = (_data - ft.fs.mu) @ scale
+    ft.data[ft.mask, :, :] = _data.reshape(shape_orig, order='F')
---
+++ test/data/test_file_tree.py
@@ -0,0 +1,80 @@
+import os
+import string
+
+import pytest
+
+from arba.data import *
+
+
+@pytest.fixture
+def file_tree(mu=np.array((0, 0)), cov=None, shape=(3, 5, 7), n=10,
+              seed=1):
+    """ builds files of random noise in 2d, returns file_tree
+
+    Args:
+        mu (np.array): mean of features
+        cov (np.array): covariance of features
+        shape (tuple): shape of images
+        n (int): number of img in tree
+    """
+
+    np.random.seed(seed)
+    d = len(mu)
+    cov = np.diag(np.array(range(d)) + 1)
+
+    def write_nii(idx):
+        """ returns dict of images, one per feature
+        """
+        # build array
+        x = np.random.multivariate_normal(mean=mu, cov=cov, size=shape)
+
+        # get temporary file
+        h, f_template = tempfile.mkstemp(suffix=f'_feat_sbj{idx}.nii.gz')
+        os.close(h)
+
+        # write output
+        f_dict = dict()
+        num_feat = len(mu)
+        for feat_idx, feat in enumerate(string.ascii_uppercase[:num_feat]):
+            f = str(f_template).replace('feat', feat)
+            img = nib.Nifti1Image(x[:, :, :, feat_idx], affine=np.eye(4))
+            img.to_filename(f)
+            f_dict[feat] = f
+
+        return f_dict
+
+    # build file tree
+    sbj_feat_file_tree = dict()
+    for idx in range(n):
+        sbj_feat_file_tree[idx] = write_nii(idx)
+
+    yield FileTree(sbj_feat_file_tree, fnc_list=[scale_normalize])
+
+    # delete files
+    for sbj, d in sbj_feat_file_tree.items():
+        for feat, f in d.items():
+            os.remove(str(f))
+
+
+def test_all(file_tree):
+    assert len(file_tree) == 10, 'file_tree.__len__ error'
+
+    mask = Mask(np.ones(file_tree.ref.shape), ref=file_tree.ref)
+    pc = PointCloud.from_mask(mask)
+
+    with file_tree:
+        # check space
+        assert file_tree.mask == mask, 'invalid mask after load'
+        assert file_tree.pc == pc, 'invalid pc after load'
+
+        # original data should have unequal variance
+        assert np.allclose(np.diag(file_tree.fs.cov),
+                           np.array((1, 2)), rtol=1e-1), 'data not unit scaled'
+
+        # compute feat_stat of data, should be 0 mean, unit variance
+        _data = file_tree.data[file_tree.mask, :, :]
+        shape = (len(file_tree.pc) * file_tree.num_sbj, file_tree.d)
+        _data = _data.reshape(shape, order='F')
+        fs = FeatStat.from_array(_data.T)
+        assert np.allclose(fs.mu, 0), 'data not centered'
+        assert np.allclose(np.diag(fs.cov), 1), 'data not unit scale'
