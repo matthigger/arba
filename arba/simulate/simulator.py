3ef4b1a601e5dec0d9858a18a21718c80c7b36c5
matt higger <matt.higger@gmail.com>
2019-03-18 12:55:55 -0400

    update

--- arba/simulate/simulator.py
+++ arba/simulate/simulator.py
@@ -1,31 +1,19 @@
-import copy
 import pathlib
 import random
 import shutil
-from collections import defaultdict
 
-import nibabel as nib
 import numpy as np
 from tqdm import tqdm
 
 from mh_pytools import file
 from mh_pytools import parallel
 from .effect import Effect
-from ..seg_graph import run_arba_cv, run_arba_permute
-from ..space import PointCloud, sample_mask, sample_mask_min_var
+from ..seg_graph import FeatStat, PermuteARBA
+from ..space import sample_mask, sample_mask_min_var, PointCloud
 
 
 class Simulator:
     """ manages sampling effects and detecting via methods
-
-    todo: a bit monolithic ... should be refactored
-
-    Attributes:
-        folder (Path): location of output
-        file_tree (FileTree): full file tree of all healthy sbj
-
-        p_effect (float): percentage of sbj which have effect applied
-        effect_list (list): list of effects to apply
     """
 
     grp_effect = 'EFFECT'
@@ -34,8 +22,8 @@ class Simulator:
     f_performance = 'performance_stats.p.gz'
 
     def __init__(self, folder, file_tree, p_effect=.5, effect_shape='min_var',
-                 verbose=True, par_flag=True, f_rba=None, tfce_num_perm=5000,
-                 alpha=.05, tfce_flag=True, vba_flag=True):
+                 verbose=True, par_flag=True, f_rba=None, num_perm=5000,
+                 alpha=.05, tfce_flag=True, vba_flag=True, active_rad=None):
         self.folder = pathlib.Path(folder)
         if self.folder.exists():
             shutil.rmtree(self.folder)
@@ -43,125 +31,112 @@ class Simulator:
 
         # split into two file_trees
         self.file_tree = file_tree
-        ft_eff, ft_null = file_tree.split(p=p_effect)
-        self.ft_dict = {self.grp_effect: ft_eff,
-                        self.grp_null: ft_null}
         self.verbose = verbose
         self.effect_shape = effect_shape
         self.effect_list = list()
         self.par_flag = par_flag
+        self.active_rad = active_rad
+
+        # split defines who is in the effect grp
+        n_effect = int(self.file_tree.num_sbj * p_effect)
+        ones_idx = np.random.choice(range(self.file_tree.num_sbj),
+                                    size=n_effect,
+                                    replace=False)
+        self.split = tuple(idx in ones_idx
+                           for idx in range(self.file_tree.num_sbj))
 
         # comparison parameters
         self.f_rba = f_rba
         self.tfce_flag = tfce_flag
         self.vba_flag = vba_flag
-        self.tfce_num_perm = tfce_num_perm
+        self.num_perm = num_perm
         self.alpha = alpha
 
     def build_effect_list(self, radius=None, num_vox=None, seg_array=None,
                           seed=1):
-
-        self.file_tree.load(verbose=self.verbose)
 
         # reset seed
         if seed is not None:
             np.random.seed(seed)
             random.seed(seed)
 
-        # build input list
-        arg_list = list()
-        if self.effect_shape == 'min_var':
-            fnc_sample_mask = sample_mask_min_var
-            # minimum variance effect regions
-            for n in num_vox:
-                d = {'ijk_fs_dict': self.file_tree.ijk_fs_dict,
-                     'ref': self.file_tree.ref,
-                     'num_vox': n}
-                arg_list.append(d)
+        with self.file_tree.loaded():
 
-        elif self.effect_shape == 'cube':
-            # cubic effect regions
-            fnc_sample_mask = sample_mask
-            if (radius is None) == (num_vox is None):
-                raise AttributeError('either radius xor num_vox required')
-            elif radius is not None:
-                for rad in radius:
-                    d = {'prior_array': self.file_tree.mask,
-                         'ref': self.file_tree.ref,
-                         'radius': rad,
-                         'seg_array': seg_array}
-                    arg_list.append(d)
-            else:
+            # build input list
+            arg_list = list()
+            if self.effect_shape == 'min_var':
+                fnc_sample_mask = sample_mask_min_var
+                # minimum variance effect regions
                 for n in num_vox:
-                    d = {'prior_array': self.file_tree.mask,
-                         'ref': self.file_tree.ref,
-                         'num_vox': n,
-                         'seg_array': seg_array}
+                    d = {'file_tree': self.file_tree,
+                         'num_vox': n}
                     arg_list.append(d)
 
-        # sample mask
-        tqdm_dict = {'desc': 'sample effect mask',
-                     'disable': not self.verbose}
-        if self.par_flag:
-            mask_list = parallel.run_par_fnc(fnc_sample_mask, arg_list,
-                                             desc=tqdm_dict['desc'])
-        else:
-            mask_list = list()
-            for d in tqdm(arg_list, **tqdm_dict):
-                mask_list.append(fnc_sample_mask(**d))
+            elif self.effect_shape == 'cube':
+                # cubic effect regions
+                fnc_sample_mask = sample_mask
+                if (radius is None) == (num_vox is None):
+                    raise AttributeError('either radius xor num_vox required')
+                elif radius is not None:
+                    for rad in radius:
+                        d = {'prior_array': self.file_tree.mask,
+                             'ref': self.file_tree.ref,
+                             'radius': rad,
+                             'seg_array': seg_array}
+                        arg_list.append(d)
+                else:
+                    for n in num_vox:
+                        d = {'prior_array': self.file_tree.mask,
+                             'ref': self.file_tree.ref,
+                             'num_vox': n,
+                             'seg_array': seg_array}
+                        arg_list.append(d)
 
-        # build effects (such that their locations are constant across t2)
-        self.effect_list = list()
-        for mask in mask_list:
-            # compute feat_stat in mask
-            pc = PointCloud.from_mask(mask)
-            fs = sum(self.file_tree.ijk_fs_dict[ijk] for ijk in pc)
+            # sample mask
+            tqdm_dict = {'desc': 'sample effect mask',
+                         'disable': not self.verbose}
+            if self.par_flag:
+                mask_list = parallel.run_par_fnc(fnc_sample_mask, arg_list,
+                                                 desc=tqdm_dict['desc'])
+            else:
+                mask_list = list()
+                for d in tqdm(arg_list, **tqdm_dict):
+                    mask_list.append(fnc_sample_mask(**d))
 
-            # t2 below is placeholder, it is reset with each run
-            e = Effect.from_fs_t2(fs=fs, mask=mask, t2=1)
-            self.effect_list.append(e)
+            # build effects (such that their locations are constant across t2)
+            self.effect_list = list()
+            for mask in mask_list:
+                pc = PointCloud.from_mask(mask)
+                fs = sum(self.file_tree.get_fs(ijk) for ijk in pc)
 
-        # delete voxel wise stats
-        self.file_tree.unload()
+                # t2 below is placeholder, it is reset with each run
+                e = Effect.from_fs_t2(fs=fs, mask=mask, t2=1)
+                self.effect_list.append(e)
 
-    def run_effect_prep(self, effect, t2=None, active_rad=None, **kwargs):
+    def run_effect_permute(self, effect, t2, folder=None, **kwargs):
         # get mask of active area
         mask_active = self.file_tree.mask
-        if active_rad is not None:
+        if self.active_rad is not None:
             # only work in a dilated region around the effect
-            mask_eff_dilated = effect.mask.dilate(active_rad)
+            mask_eff_dilated = effect.mask.dilate(self.active_rad)
             mask_active = np.logical_and(mask_eff_dilated, mask_active)
+        pc = PointCloud.from_mask(mask_active)
 
         # set scale of effect
         if t2 is not None:
             effect.t2 = t2
 
-        # build effect dict
-        grp_effect_dict = {self.grp_effect: effect}
+        # modify file_tree to specific effect
+        self.file_tree.split_effect_dict = {self.split: effect}
+        self.file_tree.pc = pc
+        self.file_tree.mask = mask_active
 
-        return mask_active, grp_effect_dict
+        # sample random split
+        permuteARBA = PermuteARBA(self.file_tree)
+        permuteARBA.run(self.split, n=self.num_perm, folder=folder,
+                        **kwargs)
 
-    def run_effect_permute(self, effect, folder, **kwargs):
-        mask, grp_effect_dict = self.run_effect_prep(effect, **kwargs)
-
-        run_arba_permute(mask=mask,
-                         grp_effect_dict=grp_effect_dict,
-                         folder=folder / 'arba_permute',
-                         ft_dict=self.ft_dict,
-                         alpha=self.alpha,
-                         **kwargs)
-
-    def run_effect_cv(self, effect, folder, **kwargs):
-        mask, grp_effect_dict = self.run_effect_prep(effect, **kwargs)
-
-        run_arba_cv(mask=mask,
-                    grp_effect_dict=grp_effect_dict,
-                    folder=folder / 'arba_cv',
-                    ft_dict=self.ft_dict,
-                    alpha=self.alpha,
-                    **kwargs)
-
-    def run(self, t2_list, **kwargs):
+    def run(self, t2_list):
 
         # build arg_list
         arg_list = list()
@@ -176,126 +151,18 @@ class Simulator:
                 d = {'effect': effect,
                      't2': t2,
                      'verbose': self.verbose and (not self.par_flag),
-                     'folder': folder}
-                d.update(kwargs)
+                     'folder': folder,
+                     'par_flag': False}
                 arg_list.append(d)
 
         # run
         desc = f'simulating effects'
         if self.par_flag:
-            parallel.run_par_fnc('run_effect_cv', arg_list=arg_list,
+            parallel.run_par_fnc('run_effect_permute', arg_list=arg_list,
                                  obj=self, desc=desc)
         if not self.par_flag:
             for d in tqdm(arg_list, desc=desc, disable=not self.verbose):
-                self.run_effect_cv(**d)
-
-    def run_tfce(self, folder):
-        # load saved file tree
-        ft_dict = get_f(folder, 'ft_dict_.p.gz')
-        ft0, ft1 = ft_dict[Simulator.grp_null], \
-                   ft_dict[Simulator.grp_effect]
-
-        # compute tfce
-        folder_tfce = folder / 'tfce'
-        folder_tfce.mkdir(exist_ok=True)
-        f_sig = folder_tfce / self.s_mask_sig.format(method='tfce')
-        raise NotImplementedError
-        _, f_sig_list = compute_tfce((ft0, ft1), alpha=self.alpha,
-                                     folder=folder_tfce,
-                                     f_data=folder_tfce / 't2.nii.gz',
-                                     num_perm=self.tfce_num_perm)
-        shutil.copy(f_sig_list[1], str(f_sig))
-
-    def run_vba_rba(self, folder):
-        # load saved file tree
-        ft_dict = get_f(folder, 'ft_dict_.p.gz')
-        ft0, ft1 = ft_dict[Simulator.grp_null], \
-                   ft_dict[Simulator.grp_effect]
-        ft0.load(load_ijk_fs=True, load_data=False)
-        ft1.load(load_ijk_fs=True, load_data=False)
-
-        sg_hist_seg = get_f(folder, 'sg_hist_seg.p.gz')
-        sg_vba_test, _, _ = next(iter(sg_hist_seg))
-        sg_vba = sg_vba_test.from_ft_dict(ft_dict)
-        sg_dict = {'vba': sg_vba}
-
-        if self.f_rba is not None:
-            sg_dict['rba'] = copy.deepcopy(sg_vba)
-            sg_dict['rba'].merge_by_atlas(self.f_rba)
-
-        # output masks of detected volumes
-        for method, sg in sg_dict.items():
-            folder_method = folder / method
-            folder_method.mkdir(exist_ok=True)
-            sg_sig = sg.get_sig(alpha=self.alpha)
-            f_out = folder_method / self.s_mask_sig.format(method=method)
-            sg_sig.to_nii(f_out=f_out,
-                          ref=ft0.ref,
-                          fnc=lambda r: np.uint8(1),
-                          background=np.uint8(0))
-
-    def get_performance(self, folder):
-        # load effect
-        effect = get_f(folder, f'effect_{self.grp_effect}.p.gz')
-
-        # compute performance
-        method_ss_dict = dict()
-        for folder_method in folder.glob('*'):
-            if not folder_method.is_dir():
-                continue
-
-            method = folder_method.name
-            f_estimate = folder_method / self.s_mask_sig.format(method=method)
-            estimate = nib.load(str(f_estimate)).get_data()
-            mask = self.file_tree.mask
-            method_ss_dict[method] = effect.get_sens_spec(estimate=estimate,
-                                                          mask=mask)
-
-        f_out = folder / 'detect_stats.txt'
-        with open(str(f_out), 'w') as f:
-            for method, (sens, spec) in sorted(method_ss_dict.items()):
-                print(f'{method}: sens {sens:.2f} spec {spec:.2f}', file=f)
-
-        t2_round = round(effect.t2, 4)
-        effect_size = int(effect.mask.sum())
-        return (t2_round, effect_size), method_ss_dict
-
-    def _run_effect_comparison(self, folder):
-        if self.tfce_flag:
-            self.run_tfce(folder)
-        if self.vba_flag:
-            self.run_vba_rba(folder)
-        return self.get_performance(folder)
-
-    def run_effect_comparison(self):
-
-        # find relevant folders, build inputs to run()
-        arg_list = list()
-        for folder in sorted(self.folder.glob('*t2*')):
-            if not folder.is_dir():
-                continue
-            arg_list.append({'folder': folder})
-
-        desc = 'compute rba, vba, tfce and performance per effect'
-        fnc = lambda: defaultdict(list)
-        t2size_method_ss_tree = defaultdict(fnc)
-        if self.par_flag:
-            res = parallel.run_par_fnc('_run_effect_comparison', arg_list,
-                                       desc=desc, obj=self)
-            for maha_size, method_ss_dict in res:
-                for method, ss in method_ss_dict.items():
-                    t2size_method_ss_tree[maha_size][method].append(ss)
-
-        else:
-            for d in tqdm(arg_list, desc=desc):
-                maha_size, method_ss_dict = self._run_effect_comparison(**d)
-                for method, ss in method_ss_dict.items():
-                    t2size_method_ss_tree[maha_size][method].append(ss)
-        # save
-        f_out = self.folder / self.f_performance
-        file.save(dict(t2size_method_ss_tree), f_out)
-
-        return t2size_method_ss_tree
+                self.run_effect_permute(**d)
 
 
 def get_f(folder, f):
--- scripts/simulate_eff/run.py
+++ scripts/simulate_eff/run.py
@@ -1,32 +1,32 @@
+import shutil
 from collections import defaultdict
 
-import numpy as np
-
+from arba.data import FileTree, scale_normalize
 from arba.plot import plot_performance
-from arba.seg_graph import FileTree
 from arba.simulate import simulator
 from mh_pytools import file
 from pnl_data.set.hcp_100 import get_name, folder
 
 #####################################################################
-t2_list = np.logspace(-1, 1, 9)
-# t2_list = [1]
+# t2_list = np.logspace(-1, 1, 9)
+t2_list = [10]
 
-num_vox = [250] * 15
+num_vox = [250] * 1
 # num_vox = np.geomspace(10, 5000, num=11).astype(int)
 # num_vox = list(num_vox) * 100
 x_axis = 'T-squared'
 
 active_rad = 5
 feat_list = ['FA', 'MD']
-par_flag = True
+par_flag = False
 effect_shape = 'cube'  # either 'cube' or 'min_var'
-tfce_num_perm = 100
+num_perm = 3
 tfce_flag = False
 vba_flag = False
 
 s_feat = '-'.join(feat_list)
 folder_out = folder / 'result' / f'ward_test_cube_det'
+shutil.rmtree(str(folder_out))
 folder_out.mkdir(exist_ok=True, parents=True)
 
 folder_data = folder / 'to_100307_low_res'
@@ -41,26 +41,28 @@ for feat in feat_list:
             continue
         sbj_feat_file_tree[get_name(f.stem)][feat] = f
 
-file_tree = FileTree(sbj_feat_file_tree=sbj_feat_file_tree)
+file_tree = FileTree(sbj_feat_file_tree=sbj_feat_file_tree,
+                     fnc_list=[scale_normalize])
 
 #####################################################################
 # init simulator
-sim = simulator.Simulator(file_tree=file_tree, folder=folder_out,
-                          par_flag=par_flag, effect_shape=effect_shape,
-                          tfce_num_perm=tfce_num_perm, tfce_flag=tfce_flag,
-                          vba_flag=vba_flag)
+with file_tree.loaded():
+    sim = simulator.Simulator(file_tree=file_tree, folder=folder_out,
+                              par_flag=par_flag, effect_shape=effect_shape,
+                              num_perm=num_perm, tfce_flag=tfce_flag,
+                              vba_flag=vba_flag, active_rad=active_rad)
 
-# build effects
-sim.build_effect_list(num_vox=num_vox)
+    # build effects
+    sim.build_effect_list(num_vox=num_vox)
 
-# save
-file.save(sim, folder_out / 'sim.p.gz')
+    # save
+    file.save(sim, folder_out / 'sim.p.gz')
 
-# run arba
-sim.run(t2_list, active_rad=active_rad)
+    # run arba
+    sim.run(t2_list)
 
-# run comparison methods
-sim.run_effect_comparison()
+    # run comparison methods
+    sim.run_effect_comparison()
 
 # plot performance
 plot_performance(sim.folder / sim.f_performance, x_axis=x_axis)
