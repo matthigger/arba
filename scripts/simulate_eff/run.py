4cfc8865d9078a3552387b94c5fb71c9c1996306
matt higger <matt.higger@gmail.com>
2019-03-20 08:40:15 -0400

    run - groom

--- scripts/simulate_eff/run.py
+++ scripts/simulate_eff/run.py
@@ -1,3 +1,4 @@
+import shutil
 from collections import defaultdict
 
 import numpy as np
@@ -9,10 +10,10 @@ from mh_pytools import file
 from pnl_data.set.hcp_100 import get_name, folder
 
 #####################################################################
-t2_list = np.logspace(-1, 1, 2)
-# t2_list = [10]
+t2_list = np.logspace(-1, 1, 4)
+# t2_list = [.1]
 
-num_vox = [50] * 1
+num_vox = [50] * 6
 # num_vox = np.geomspace(10, 5000, num=11).astype(int)
 # num_vox = list(num_vox) * 100
 x_axis = 'T-squared'
@@ -21,18 +22,24 @@ active_rad = 5
 feat_list = ['FA', 'MD']
 par_flag = True
 effect_shape = 'cube'  # either 'cube' or 'min_var'
-num_perm = 5
-tfce_flag = False
-vba_flag = False
+num_perm = 3
+tfce_flag = True
 
-s_feat = '-'.join(feat_list)
-folder_out = folder / 'result' / f'ward_test_cube_det'
-# shutil.rmtree(str(folder_out))
+print_tree = True
+print_image = True
+print_hist = True
+
+# make output folder
+n = len(t2_list) * len(num_vox)
+s_feat = ''.join(feat_list)
+folder_out = folder / 'result' / f'{n}_{s_feat}_{effect_shape}'
+if folder_out.exists():
+    shutil.rmtree(str(folder_out))
 folder_out.mkdir(exist_ok=True, parents=True)
 
-folder_data = folder / 'to_100307_low_res'
 #####################################################################
 # build file_tree
+folder_data = folder / 'to_100307_low_res'
 sbj_feat_file_tree = defaultdict(dict)
 for feat in feat_list:
     for f in folder_data.glob(f'*{feat}.nii.gz'):
@@ -50,8 +57,7 @@ file_tree = FileTree(sbj_feat_file_tree=sbj_feat_file_tree,
 with file_tree.loaded():
     sim = Simulator(file_tree=file_tree, folder=folder_out, par_flag=par_flag,
                     effect_shape=effect_shape, num_perm=num_perm,
-                    tfce_flag=tfce_flag, vba_flag=vba_flag,
-                    active_rad=active_rad)
+                    tfce_flag=tfce_flag, active_rad=active_rad)
 
     # build effects
     sim.build_effect_list(num_vox=num_vox)
@@ -60,10 +66,10 @@ with file_tree.loaded():
     file.save(sim, folder_out / 'sim.p.gz')
 
     # run arba
-    sim.run(t2_list)
+    sim.run(t2_list, print_tree=print_tree, print_image=print_image,
+            print_hist=print_hist)
 
     # run comparison methods
-    sim = file.load(folder_out / 'sim.p.gz')
     f_performance = sim.get_performance()
 
 plot_performance(f_performance, x_axis=x_axis)
