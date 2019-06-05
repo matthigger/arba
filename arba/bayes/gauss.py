a07f79842fd62c24c3a902c5cc615f6bd5c0f9bb
matt higger <matt.higger@gmail.com>
2019-06-05 15:49:39 -0400

    minor

--- arba/bayes/gauss.py
+++ arba/bayes/gauss.py
@@ -2,26 +2,23 @@ import numpy as np
 import scipy.stats
 
 
-def get_gauss_and_min_delta(x, alpha=.05):
+def get_lower_bnd(mu, cov, alpha=.05):
     """ estimate prob of delta as a gaussian, returns lower bound on delta
 
     by lower bound we mean the value of delta which is less probable than (1 -
     alpha) of points (and has min norm on that ellipse)
 
     Args:
-        x (np.array): (num_samples, d) samples drawn from the distribution of
-                      delta
-        alpha (float): min probability to determine bound
+        mu (np.array): mean
+        cov (np.array): cov
+        alpha (flaot): confidence level
 
     Returns:
-        mv_norm (scipy.stats.multivariate_normal):
         lower_bnd (np.array):
     """
-
-    # build mv normal
-    mu = np.mean(x, axis=0)
-    cov = np.cov(x.T)
-    mv_norm = scipy.stats.multivariate_normal(mean=mu, cov=cov)
+    # ensure the proper shape
+    mu = np.atleast_1d(mu)
+    cov = np.atleast_2d(cov)
 
     # find mahalanobis distance maha such that 1 - alpha of prob mass has maha
     # less than or equal to it
@@ -31,13 +28,13 @@ def get_gauss_and_min_delta(x, alpha=.05):
     cov_inv = np.linalg.inv(cov)
     offset = -mu
     _maha = offset @ cov_inv @ offset
-    lower_bnd = offset * np.sqrt(maha / _maha) + mu
-    assert np.isclose(maha,
-                      (lower_bnd - mu) @ cov_inv @ (lower_bnd - mu)), \
-        'compute error'
+    offset *= np.sqrt(maha / _maha)
+    lower_bnd = offset + mu
+    assert np.isclose(maha, offset @ cov_inv @ offset), 'compute error'
 
-    # set lower_bnd to delta=0 is within the 1-alpha most probable points
+    # is the origin in the ellipse which contains 95% of the prob?
     if np.linalg.norm(mu) <= np.linalg.norm(mu - lower_bnd):
+        # if so, then the lower bound is 0
         lower_bnd = np.zeros(mu.shape)
 
-    return mv_norm, lower_bnd
+    return lower_bnd
--- scripts/sim_ml_bayes.py
+++ scripts/sim_ml_bayes.py
@@ -2,28 +2,32 @@ import pathlib
 import tempfile
 
 import nibabel as nib
-import arba
 import numpy as np
-import os
-from mh_pytools import file
+
+import arba
+from mh_pytools import file, parallel
 
 # file tree
 n_sbj = 10
 mu = (0, 0)
 cov = np.eye(2)
-shape = 10, 10, 10
-
+shape = 5, 5, 1
+t
 # effect
 mask = np.zeros(shape)
-mask[3:7, 3:7, 3:7] = 1
-offset = (10, 10)
+mask[2:4, 2:4, 0] = 1
+offset = (1, 1)
+
+# bayes threshold
+alpha = .05
 
 # build file tree
 folder = pathlib.Path(tempfile.TemporaryDirectory().name)
-os.mkdir(str(folder))
 print(folder)
+_folder = folder / 'data_orig'
+_folder.mkdir(exist_ok=True, parents=True)
 ft = arba.data.SynthFileTree(n_sbj=n_sbj, shape=shape, mu=mu, cov=cov,
-                             folder=folder)
+                             folder=_folder)
 
 # build effect
 effect = arba.simulate.Effect(mask=mask, offset=offset)
@@ -48,3 +52,40 @@ with ft.loaded(split_eff_list=[(split, effect)]):
     # save
     f = folder / 'sg_hist.p.gz'
     file.save(sg_hist, f)
+
+    # run bayes on each volume, record mv_norm and lower_bnd
+    arg_list = list()
+    merge_record = sg_hist.merge_record
+    for node in merge_record.nodes:
+        # get mask
+        pc = merge_record.get_pc(node)
+        mask = pc.to_mask()
+
+        #
+        data0, data1 = arba.bayes.get_data(file_tree=ft, mask=mask,
+                                           split=split)
+        d = {'data0': data0,
+             'data1': data1,
+             'node': node}
+
+        arg_list.append(d)
+
+
+def fnc(data0, data1, node):
+    # run bayes
+    _, trace = arba.bayes.estimate_delta(data0=data0, data1=data1, cores=1)
+
+    x = trace['delta']
+    mu = np.mean(x, axis=0)
+    cov = np.cov(x.T)
+
+    return node, (mu, cov)
+
+
+# run par func
+res_out = parallel.run_par_fnc(fnc, arg_list, desc='bayes per region')
+node_mu_cov_dict = dict(res_out)
+
+# save
+f = folder / 'node_mu_cov_dict.p.gz'
+file.save(node_mu_cov_dict, f)
