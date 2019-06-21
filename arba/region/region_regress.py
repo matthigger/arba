28bfa092248cfd4951e0a6dd6ab32c5e7fd06d91
matt higger <matt.higger@gmail.com>
2019-06-21 11:51:05 -0400

    revise - region_regress - split _compute_beta_err() into two methods: fit(), get_err()

--- arba/region/region_regress.py
+++ arba/region/region_regress.py
@@ -1,11 +1,24 @@
+import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+
+sns.set(font_scale=1.2)
+
+
+def append_col_one(x):
+    if len(x.shape) == 1:
+        x = np.atleast_2d(x).T
+    num_sbj = x.shape[0]
+    ones = np.atleast_2d(np.ones(num_sbj)).T
+    return np.hstack((x, ones))
 
 
 class RegionRegress:
     """ regression, from sbj features to img features, on some volume
 
     Class Attributes:
-        feat_sbj (np.array): (num_sbj, dim_sbj) subject features
+        feat_sbj (np.array): (num_sbj, dim_sbj + 1) subject features (ones col
+                             appended)
         pseudo_inv (np.array): pseudo inverse (used to compute beta)
 
     Instance Attributes:
@@ -22,31 +35,45 @@ class RegionRegress:
 
     @staticmethod
     def from_data(pc_ijk, file_tree):
+        assert file_tree.is_loaded, 'file tree must not loaded'
         mask = pc_ijk.to_mask()
         feat_img = np.mean(file_tree.data[mask, :, :], axis=0)
         return RegionRegress(feat_img=feat_img, pc_ijk=pc_ijk)
 
     @classmethod
     def set_feat_sbj(cls, feat_sbj):
-        cls.feat_sbj = feat_sbj
+        cls.feat_sbj = np.atleast_2d(feat_sbj)
+        cls.feat_sbj = append_col_one(cls.feat_sbj)
 
-        cls.pseudo_inv = np.linalg.inv(feat_sbj.T @ feat_sbj) @ feat_sbj.T
+        x = cls.feat_sbj
+        cls.pseudo_inv = np.linalg.inv(x.T @ x) @ x.T
 
-    def _compute_beta_err_cov(self):
+    def fit(self, feat_img):
         assert self.feat_sbj is not None, \
             'call RegRegress.set_feat_sbj() needed'
 
-        self.beta = self.pseudo_inv @ self.feat_img
-        estimate = self.feat_img @ self.beta.T
-        delta = self.feat_sbj - estimate
-        self.err_cov = np.cov(delta.T, ddof=0)
-        self.err_cov_det = np.linalg.det(self.err_cov)
+        self.beta = self.pseudo_inv @ feat_img
+
+    def get_err(self, feat_img):
+        if feat_img is None:
+            feat_img = self.feat_img
+
+        delta = feat_img - self.project(self.feat_sbj, append_flag=False)
+
+        err_cov = np.atleast_2d(np.cov(delta.T, ddof=0))
+        r2 = 1 - np.linalg.det(err_cov) / \
+             np.linalg.det(self.feat_img_cov)
+
+        return err_cov, r2
 
     def __init__(self, feat_img, pc_ijk):
-        self.feat_img = feat_img
+        self.feat_img = np.atleast_2d(feat_img)
+        self.feat_img_cov = np.atleast_2d(np.cov(self.feat_img.T, ddof=0))
+
         self.pc_ijk = pc_ijk
 
-        self._compute_beta_err_cov()
+        self.fit(feat_img)
+        self.err_cov, self.r2 = self.get_err(feat_img)
 
     def __add__(self, other):
         # allows use of sum(reg_iter)
@@ -65,10 +92,11 @@ class RegionRegress:
     def get_error(reg_1, reg_2, reg_u=None):
         if reg_u is None:
             reg_u = reg_1 + reg_2
-        delta = reg_u.err_cov_det * len(reg_u) - \
-                reg_1.err_cov_det * len(reg_1) - \
-                reg_2.err_cov_det * len(reg_2)
-        return delta
+        delta = reg_u.r2 * len(reg_u) - \
+                reg_1.r2 * len(reg_1) - \
+                reg_2.r2 * len(reg_2)
+
+        return np.abs(delta)
 
     __radd__ = __add__
 
@@ -77,3 +105,23 @@ class RegionRegress:
 
     def __lt__(self, other):
         return len(self) < len(other)
+
+    def project(self, feat_sbj, append_flag=True):
+        if append_flag:
+            feat_sbj = append_col_one(feat_sbj)
+        return feat_sbj @ self.beta
+
+    def plot(self, sbj_feat_label='sbj_feat', img_feat_label='img_feat'):
+        # line below note: we append column of 1's ...
+        assert self.feat_sbj.shape[1] == 2, 'only valid for scalar feat_sbj'
+        assert self.feat_img.shape[1] == 1, 'only valid for scalar feat_img'
+
+        feat_sbj = self.feat_sbj[:, 0]
+        feat_sbj_line = np.array((min(feat_sbj), max(feat_sbj)))
+        feat_img_line = self.project(feat_sbj_line, append_flag=True)
+
+        plt.scatter(feat_sbj, self.feat_img)
+        plt.suptitle(f'r2={self.r2:.2f}, size={len(self)} vox')
+        plt.plot(feat_sbj_line, feat_img_line)
+        plt.xlabel(sbj_feat_label)
+        plt.ylabel(img_feat_label)
