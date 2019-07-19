c0b93aa0df302f9301193888f4a9017620108d87
matt higger <matt.higger@gmail.com>
2019-07-19 12:20:51 -0400

    revise - scripts.regress.ex - now performs permutation testing, prints only significant regions

--- scripts/regress/ex.py
+++ scripts/regress/ex.py
@@ -2,7 +2,9 @@ import pathlib
 import shutil
 import tempfile
 
+import matplotlib.pyplot as plt
 import numpy as np
+from scipy.spatial.distance import dice
 from tqdm import tqdm
 
 import arba
@@ -12,7 +14,10 @@ dim_img = 1
 
 mu_sbj = np.zeros(dim_sbj)
 sig_sbj = np.eye(dim_sbj)
-num_sbj = 25
+num_sbj = 50
+
+cutoff_perc = 95
+num_perm = 10
 
 shape = 6, 6, 6
 mask = np.zeros(shape)
@@ -96,7 +101,7 @@ def permute_run(target_fnc, n=5000, max_flag=True, **kwargs):
 
 
 with file_tree.loaded():
-    val_list = permute_run(n=10, target_fnc=weighted_r2, max_flag=True,
+    val_list = permute_run(n=num_perm, target_fnc=weighted_r2, max_flag=True,
                            feat_sbj=feat_sbj, file_tree=file_tree)
 
     sg_hist = run(feat_sbj=feat_sbj, file_tree=file_tree)
@@ -116,6 +121,14 @@ with file_tree.loaded():
                                                  file_tree=file_tree,
                                                  reg_cls=arba.region.RegionRegress)
 
+cutoff = np.percentile(val_list, cutoff_perc)
+plt.hist(val_list, bins=100)
+plt.xlabel('max n r^2 per permutations')
+plt.suptitle(f'{len(val_list)} permutations')
+arba.plot.save_fig(folder / 'permute_nr2.pdf')
+
+reg_list = [r for r in reg_list if weighted_r2(r) > cutoff]
+
 sg_hist.merge_record.plot_size_v(weighted_r2, label='n * r2', mask=mask,
                                  log_y=True)
 arba.plot.save_fig(folder / 'size_v_nr2.pdf')
@@ -123,7 +136,18 @@ arba.plot.save_fig(folder / 'size_v_nr2.pdf')
 sg_hist.merge_record.plot_size_v(mse, label='mse', mask=mask)
 arba.plot.save_fig(folder / 'size_v_mse.pdf')
 
-for idx, r in enumerate(reg_list[:3]):
+for idx, r in enumerate(reg_list):
     r.plot()
     arba.plot.save_fig(f_out=folder / f'reg_{idx}_scatter.pdf')
     r.pc_ijk.to_mask().to_nii(f_out=folder / f'reg_{idx}_mask.nii.gz')
+
+mask_detected = sum(r.pc_ijk.to_mask() for r in reg_list).astype(bool)
+dice_score = 1 - dice(mask_detected.flatten(), mask.flatten())
+with open(str(folder / 'dice.txt'), 'w') as f:
+    print(f'dice is {dice_score:.3f}', file=f)
+    print(f'target region size: {mask.sum()}', file=f)
+    print(f'detected region size: {mask_detected.sum()}', file=f)
+    print(f'true detected region size: {(mask & mask_detected).sum()}', file=f)
+    print(f'false detected region size: {(~mask & mask_detected).sum()}', file=f)
+
+print(folder)
