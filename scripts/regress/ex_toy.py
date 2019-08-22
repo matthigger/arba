24d58643fa1caf9b26e12346576fead981f94190
matt higger <matt.higger@gmail.com>
2019-08-22 13:29:40 -0400

    add support for no agglomerative step in permutation (r2 only decreases with agglomeration ... no need to do it if we're looking for the max value)
    rename - regress.ex.py -> regress.ex_toy.py

--- scripts/regress/ex_toy.py
+++ scripts/regress/ex_toy.py
@@ -17,7 +17,7 @@ dim_img = 1
 # subject params
 mu_sbj = np.zeros(dim_sbj)
 sig_sbj = np.eye(dim_sbj)
-num_sbj = 50
+num_sbj = 100
 
 # imaging params
 mu_img = np.zeros(dim_img)
@@ -25,22 +25,22 @@ sig_img = np.eye(dim_img)
 shape = 6, 6, 6
 
 # detection params
-num_perm = 2
+num_perm = 50
 
 # regression params
 r2 = .5
-mask = np.zeros(shape)
-mask[2:5, 2:5, 2:5] = True
+effect_mask = np.zeros(shape)
+effect_mask[2:5, 2:5, 2:5] = True
 
 # build effect
 ref = arba.space.RefSpace(affine=np.eye(4))
-mask = arba.space.Mask(mask, ref=ref)
+effect_mask = arba.space.Mask(effect_mask, ref=ref)
 
 # build output folder
 folder = pathlib.Path(tempfile.TemporaryDirectory().name)
 folder.mkdir()
 print(folder)
-shutil.copy(__file__, folder / 'ex.py')
+shutil.copy(__file__, folder / 'ex_toy.py')
 
 # sample sbj features
 feat_sbj = np.random.multivariate_normal(mean=mu_sbj,
@@ -62,9 +62,9 @@ feat_mapper = arba.regress.FeatMapperStatic(n=dim_sbj,
 
 # build regression, impose it
 with file_tree.loaded():
-    fs = file_tree.get_fs(mask=mask)
+    fs = file_tree.get_fs(mask=effect_mask)
 eff = arba.simulate.EffectRegress.from_r2(r2=r2,
-                                          mask=mask,
+                                          mask=effect_mask,
                                           eps_img=fs.cov,
                                           cov_sbj=np.cov(feat_sbj.T, ddof=0),
                                           feat_mapper=feat_mapper)
@@ -75,8 +75,8 @@ def mse(reg, **kwargs):
     return reg.mse
 
 
-def weighted_r2(reg, **kwargs):
-    return reg.r2 * len(reg)
+def r2(reg, **kwargs):
+    return reg.r2
 
 
 def maha_zero(reg, **kwargs):
@@ -84,30 +84,35 @@ def maha_zero(reg, **kwargs):
 
 
 f_mask = folder / 'target_mask.nii.gz'
-mask.to_nii(f_mask)
+effect_mask.to_nii(f_mask)
 
-fnc_tuple = mse, maha_zero
+fnc_tuple = mse, maha_zero, r2
 with file_tree.loaded(effect_list=[eff]):
     sg_hist, sig_node_list, val_list = \
         arba.regress.run_permute(feat_sbj, file_tree,
-                                 fnc_target=maha_zero,
+                                 fnc_target=r2,
                                  save_folder=folder,
                                  max_flag=True,
                                  cutoff_perc=95,
                                  n=num_perm,
-                                 fnc_tuple=fnc_tuple)
+                                 fnc_tuple=fnc_tuple,
+                                 agg_perm_flag=False)
 
-node_mask, d_max = sg_hist.merge_record.get_node_max_dice(mask)
+node_mask, d_max = sg_hist.merge_record.get_node_max_dice(effect_mask)
 
-sg_hist.merge_record.plot_size_v(maha_zero, label='maha(0)', mask=mask,
+sg_hist.merge_record.plot_size_v(maha_zero, label='maha(0)', mask=effect_mask,
                                  log_y=True)
 arba.plot.save_fig(folder / 'size_v_maha0.pdf')
 
-sg_hist.merge_record.plot_size_v(mse, label='mse', mask=mask)
+sg_hist.merge_record.plot_size_v(r2, label='r2', mask=effect_mask,
+                                 log_y=False)
+arba.plot.save_fig(folder / 'size_v_r2.pdf')
+
+sg_hist.merge_record.plot_size_v(mse, label='mse', mask=effect_mask)
 arba.plot.save_fig(folder / 'size_v_mse.pdf')
 
 mask_estimate = arba.regress.build_mask(sig_node_list, sg_hist.merge_record)
 mask_estimate.to_nii(folder / 'mask_estimate.nii.gz')
 arba.regress.compute_print_dice(mask_estimate=mask_estimate,
-                                mask_target=mask, save_folder=folder)
+                                mask_target=effect_mask, save_folder=folder)
 print(folder)
