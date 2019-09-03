4b19c681685c79f4401970cb1402d1ae9c70ed3d
matt higger <matt.higger@gmail.com>
2019-09-03 14:26:22 -0400

    refactor - arba.permute.PermuteRegress.save() / get_pval() / get_r2_zscore() / _compute_r2_bounds()

--- arba/permute/regress.py
+++ arba/permute/regress.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from scipy.spatial.distance import dice
+from tqdm import tqdm
 
 import arba
 from mh_pytools import parallel
 from ..region import RegionRegress
-from tqdm import tqdm
 from ..seg_graph import SegGraphHistory
 
 
@@ -49,54 +49,26 @@ class PermuteRegress:
         with file_tree.loaded():
             self.run_single()
 
-            val_list = self.permute(par_flag=par_flag)
-
-            # build r2_null, has shape (num_perm, num_vox).  each column is a
-            # sorted list of upper bounds on r2 per permutation.
-            r2_null = np.vstack(val_list)
-            num_perm, num_vox = r2_null.shape
-
-            # sum and normalize
-            r2_null = np.cumsum(r2_null, axis=1)
-            r2_null = r2_null / np.arange(1, num_vox + 1)
-
-            # sort per region size
-            r2_null = np.sort(r2_null, axis=0)
-
-            # compute stats (for z)
-            mu_null = np.mean(r2_null, axis=0)
-            std_null = np.std(r2_null, axis=0)
+            self.permute(par_flag=par_flag)
 
             # compute pval + z score per node
-            node_pval_dict = dict()
-            node_z_dict = dict()
-            node_r2_dict = self.merge_record.fnc_node_val_list['r2']
+            self.node_pval_dict = dict()
+            self.node_z_dict = dict()
+            self.node_r2_dict = self.merge_record.fnc_node_val_list['r2']
             for n in self.merge_record.nodes:
-                node_size = self.merge_record.node_size_dict[n]
+                reg_size = self.merge_record.node_size_dict[n]
+                r2 = self.node_r2_dict[n]
 
-                # compute percentile
-                _r2_null = r2_null[:, node_size - 1]
-                pval = 1 - bisect_right(_r2_null, node_r2_dict[n]) / num_perm
-                node_pval_dict[n] = pval
+                self.node_pval_dict[n] = self.get_pval(r2, reg_size)
+                self.node_z_dict[n] = self.get_r2_z_score(r2, reg_size)
 
-                # compute z score
-                mu = mu_null[node_size - 1]
-                std = std_null[node_size - 1]
-                node_z_dict[n] = (node_r2_dict[n] - mu) / std
-
-            node_p_negz_dict = {n: (p, -node_z_dict[n])
-                                for n, p in node_pval_dict.items() if
+            node_p_negz_dict = {n: (p, -self.node_z_dict[n])
+                                for n, p in self.node_pval_dict.items() if
                                 p <= self.alpha}
-            sig_node_cover = self.merge_record._cut_greedy(node_p_negz_dict,
-                                                           max_flag=False)
+            self.sig_node_cover = \
+                self.merge_record._cut_greedy(node_p_negz_dict, max_flag=False)
 
-            save(self.sg_hist, sig_node_cover, node_pval_dict, node_z_dict,
-                 r2_null, folder=self.folder, mask=target_mask)
-
-        self.node_pval_dict = node_pval_dict
-        self.node_z_dict = node_z_dict
-        self.r2_null = r2_null
-        self.sig_node_cover = sig_node_cover
+            self.save()
 
     def run_single(self, _seed=None):
         """ runs a single Agglomerative Clustering run
@@ -147,71 +119,95 @@ class PermuteRegress:
             for d in tqdm(arg_list, desc='permute', disable=not self.verbose):
                 val_list.append(self.run_single_permute(**d))
 
+        self._compute_r2_bounds(val_list)
+
         # ensure that RegionRegress has appropriate feat_sbj ordering
         RegionRegress.set_feat_sbj(feat_sbj=self.feat_sbj,
                                    sbj_list=self.file_tree.sbj_list)
 
         return val_list
 
+    def get_pval(self, r2, reg_size):
+        _r2_null = self.r2_null[:, reg_size - 1]
+        return 1 - bisect_right(_r2_null, r2) / self.num_perm
 
-def save(sg_hist, sig_node_cover, node_pval_dict, node_z_dict, r2_null,
-         folder=None, mask=None, size_v_r2=True, size_v_r2_pval=True,
-         size_v_r2_z=True, size_v_r2_null=True, mask_detected=True,
-         print_node=True):
-    if folder is None:
-        folder = pathlib.Path(tempfile.mkdtemp().name)
+    def get_r2_z_score(self, r2, reg_size):
+        mu = self.mu_null[reg_size - 1]
+        std = self.std_null[reg_size - 1]
+        return (r2 - mu) / std
 
-    merge_record = sg_hist.merge_record
+    def _compute_r2_bounds(self, val_list):
+        """ computes bound on r2, per region size, for every permutation """
+        # build r2_null, has shape (num_perm, num_vox).  each column is a
+        # sorted list of upper bounds on r2 per permutation.
+        self.r2_null = np.vstack(val_list)
+        num_perm, num_vox = self.r2_null.shape
 
-    if size_v_r2:
-        merge_record.plot_size_v('r2', label='r2', mask=mask, log_y=False)
-        arba.plot.save_fig(folder / 'size_v_r2.pdf')
+        # sum and normalize
+        r2_null = np.cumsum(self.r2_null, axis=1)
+        r2_null = r2_null / np.arange(1, num_vox + 1)
 
-    if size_v_r2_pval:
-        merge_record.plot_size_v(node_pval_dict, label='pval', mask=mask,
-                                 log_y=False)
-        arba.plot.save_fig(folder / 'size_v_pval.pdf')
+        # sort per region size
+        r2_null = np.sort(r2_null, axis=0)
 
-    if size_v_r2_z:
-        merge_record.plot_size_v(node_z_dict, label='r2z', mask=mask,
-                                 log_y=False)
-        arba.plot.save_fig(folder / 'size_v_r2z_score.pdf')
+        # compute stats (for z)
+        self.mu_null = np.mean(r2_null, axis=0)
+        self.std_null = np.std(r2_null, axis=0)
 
-    if mask_detected:
-        mask_estimate = merge_record.build_mask(sig_node_cover)
-        mask_estimate.to_nii(folder / 'mask_detected.nii.gz')
+    def save(self, size_v_r2=True, size_v_r2_pval=True, size_v_r2_z=True,
+             size_v_r2_null=True, mask_detected=True, print_node=True):
 
-    if size_v_r2_null:
-        # print percentiles of r2 per size
-        sns.set(font_scale=1.2)
-        cmap = plt.get_cmap('viridis')
-        size = np.arange(1, r2_null.shape[1] + 1)
-        p_list = [50, 75, 90, 95, 99]
-        for p_idx, p in enumerate(p_list):
-            perc_line = np.percentile(r2_null, p, axis=0)
-            plt.plot(size, perc_line, label=f'{p}-th percentile',
-                     color=cmap(p_idx / len(p_list)))
-        plt.ylabel('r2')
-        plt.xlabel('size')
-        plt.gca().set_xscale('log')
-        plt.legend()
-        arba.plot.save_fig(folder / 'size_v_r2_null.pdf')
+        if size_v_r2:
+            self.merge_record.plot_size_v('r2', label='r2',
+                                          mask=self.target_mask,
+                                          log_y=False)
+            arba.plot.save_fig(self.folder / 'size_v_r2.pdf')
 
-    if mask is not None:
-        compute_print_dice(mask_estimate=mask_estimate,
-                           mask_target=mask,
-                           save_folder=folder)
+        if size_v_r2_pval:
+            self.merge_record.plot_size_v(self.node_pval_dict, label='pval',
+                                          mask=self.target_mask,
+                                          log_y=False)
+            arba.plot.save_fig(self.folder / 'size_v_pval.pdf')
 
-    if print_node:
-        for n in sig_node_cover:
-            r = merge_record.resolve_node(n,
-                                          file_tree=sg_hist.file_tree,
-                                          reg_cls=arba.region.RegionRegress)
-            r.pc_ijk.to_mask().to_nii(folder / f'node_{n}.nii.gz')
-            r.plot(img_feat_label='fa')
-            arba.plot.save_fig(folder / f'node_{n}.pdf')
+        if size_v_r2_z:
+            self.merge_record.plot_size_v(self.node_z_dict, label='r2z',
+                                          mask=self.target_mask,
+                                          log_y=False)
+            arba.plot.save_fig(self.folder / 'size_v_r2z_score.pdf')
 
-    return folder
+        if mask_detected:
+            mask_estimate = self.merge_record.build_mask(self.sig_node_cover)
+            mask_estimate.to_nii(self.folder / 'mask_detected.nii.gz')
+
+        if size_v_r2_null:
+            # print percentiles of r2 per size
+            sns.set(font_scale=1.2)
+            cmap = plt.get_cmap('viridis')
+            size = np.arange(1, self.r2_null.shape[1] + 1)
+            p_list = [50, 75, 90, 95, 99]
+            for p_idx, p in enumerate(p_list):
+                perc_line = np.percentile(self.r2_null, p, axis=0)
+                plt.plot(size, perc_line, label=f'{p}-th percentile',
+                         color=cmap(p_idx / len(p_list)))
+            plt.ylabel('r2')
+            plt.xlabel('size')
+            plt.gca().set_xscale('log')
+            plt.legend()
+            arba.plot.save_fig(self.folder / 'size_v_r2_null.pdf')
+
+        if self.target_mask is not None:
+            compute_print_dice(mask_estimate=mask_estimate,
+                               mask_target=self.target_mask,
+                               save_folder=self.folder)
+
+        if print_node:
+            for n in self.sig_node_cover:
+                r = self.merge_record.resolve_node(n,
+                                                   file_tree=self.sg_hist.file_tree,
+                                                   reg_cls=arba.region.RegionRegress)
+                r.pc_ijk.to_mask().to_nii(self.folder / f'node_{n}.nii.gz')
+                r.plot(img_feat_label='fa')
+                arba.plot.save_fig(self.folder / f'node_{n}.pdf')
 
 
 def compute_print_dice(mask_estimate, mask_target, save_folder):
