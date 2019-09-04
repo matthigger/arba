244d8e04fe52d6cd325d6f5c6a98fc06ecd83155
matt higger <matt.higger@gmail.com>
2019-09-04 10:46:44 -0400

    update - scripts.regress_ex_toy - runs multiple effects, each of which is scaled to the values in a list of r2

--- scripts/regress_ex_toy.py
+++ scripts/regress_ex_toy.py
@@ -1,7 +1,11 @@
 import shutil
 
-from arba.effect import get_effect_list, get_sens_spec
+import numpy as np
+
+from arba.effect import get_effect_list, get_sens_spec, EffectRegress
 from arba.permute import PermuteRegressVBA
+import tempfile
+import pathlib
 
 # detection params
 par_flag = True
@@ -10,30 +14,37 @@ alpha = .05
 
 # regression effect params
 shape = 5, 5, 5
-r2 = .1
+r2_vec = np.logspace(-2, -.1, 5)
+num_eff = 3
+
 effect_num_vox = 10
 
-feat_sbj, file_tree, eff_list = get_effect_list(
-    effect_num_vox=effect_num_vox,
-    shape=shape, r2=r2,
-    rand_seed=1)
+feat_sbj, file_tree, eff_list = get_effect_list(effect_num_vox=effect_num_vox,
+                                                shape=shape, r2=r2_vec[0],
+                                                rand_seed=1,
+                                                num_eff=num_eff)
 
-eff = eff_list[0]
+folder = pathlib.Path(tempfile.mkdtemp())
+shutil.copy(__file__, folder / 'regress_ex_toy.py')
 
-with file_tree.loaded(effect_list=[eff]):
-    perm_reg = PermuteRegressVBA(feat_sbj, file_tree,
-                                 num_perm=num_perm,
-                                 par_flag=par_flag,
-                                 alpha=alpha,
-                                 mask_target=eff.mask,
-                                 verbose=True)
-    estimate_dict = {'arba': perm_reg.mask_estimate}
-    estimate_dict.update(perm_reg.vba_mask_estimate_dict)
+for eff in eff_list:
+    for r2 in r2_vec:
+        eff = eff.scale_r2(r2)
 
-for label, estimate in estimate_dict.items():
-    sens, spec = get_sens_spec(target=eff.mask, estimate=estimate,
-                               mask=file_tree.mask)
-    print(f'{label}: sens {sens:.3f} spec {spec:.3f}')
+        with file_tree.loaded(effect_list=[eff]):
+            perm_reg = PermuteRegressVBA(feat_sbj, file_tree,
+                                         num_perm=num_perm,
+                                         par_flag=par_flag,
+                                         alpha=alpha,
+                                         mask_target=eff.mask,
+                                         verbose=True,
+                                         save_flag=False)
+            estimate_dict = {'arba': perm_reg.mask_estimate}
+            estimate_dict.update(perm_reg.vba_mask_estimate_dict)
 
-shutil.copy(__file__, perm_reg.folder / 'regress_ex_toy.py')
+        for label, estimate in estimate_dict.items():
+            sens, spec = get_sens_spec(target=eff.mask, estimate=estimate,
+                                       mask=file_tree.mask)
+            print(f'{label} (r2: {r2:.2e}): sens {sens:.3f} spec {spec:.3f}')
+
 print(perm_reg.folder)
