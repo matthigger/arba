4f03f5ab71f9d10f1889d14da9f31f93a9d8340f
matt higger <matt.higger@gmail.com>
2019-09-06 13:35:56 -0400

    revise - regress_ex_toy - now supports hcp dataset

--- scripts/regress_ex_toy.py
+++ scripts/regress_ex_toy.py
@@ -6,11 +6,13 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from scipy.ndimage.morphology import binary_dilation
 
 from arba.data import SynthFileTree
 from arba.effect import get_effect_list, get_sens_spec
 from arba.permute import PermuteRegressVBA
 from arba.plot import save_fig
+from pnl_data.set import hcp_100
 
 # detection params
 par_flag = True
@@ -18,38 +20,53 @@ num_perm = 24
 alpha = .05
 
 # regression effect params
-shape = 6, 6, 6
 # r2_vec = np.logspace(-2, -.5, 9)
 r2_vec = [.9]
 num_eff = 1
 dim_sbj = 1
-dim_img = 1
 num_sbj = 100
+
+str_img_data = 'hcp100'  # 'hcp100' or 'synth'
+
+mask_radius = 5
 
 effect_num_vox = 20
 
 # build dummy folder
 folder = pathlib.Path(tempfile.mkdtemp())
 shutil.copy(__file__, folder / 'regress_ex_toy.py')
+print(folder)
 
 # duild bummy images
-file_tree = SynthFileTree(num_sbj=num_sbj, shape=shape, mu=np.zeros(dim_img),
-                          cov=np.eye(dim_img), folder=folder / 'raw_data')
+if str_img_data == 'synth':
+    dim_img = 1
+    shape = 6, 6, 6
+    file_tree = SynthFileTree(num_sbj=num_sbj, shape=shape,
+                              mu=np.zeros(dim_img),
+                              cov=np.eye(dim_img), folder=folder / 'raw_data')
+elif str_img_data == 'hcp100':
+    low_res = True,
+    feat_tuple = 'fa',
+    file_tree = hcp_100.get_file_tree(lim_sbj=num_sbj,
+                                      low_res=low_res,
+                                      feat_tuple=feat_tuple)
 
-feat_sbj, eff_list = get_effect_list(file_tree=file_tree,
-                                     effect_num_vox=effect_num_vox,
-                                     r2=r2_vec[0],
-                                     rand_seed=1,
-                                     dim_sbj=dim_sbj,
-                                     num_eff=num_eff,
-                                     no_edge=True,
-                                     min_var_mask=True)
+with file_tree.loaded():
+    feat_sbj, eff_list = get_effect_list(file_tree=file_tree,
+                                         effect_num_vox=effect_num_vox,
+                                         r2=r2_vec[0],
+                                         rand_seed=1,
+                                         dim_sbj=dim_sbj,
+                                         num_eff=num_eff,
+                                         no_edge=True,
+                                         min_var_mask=True)
+    file_tree.to_nii(folder, mean_flag=True)
 
 method_r2ss_list_dict = defaultdict(list)
 for eff_idx, eff in enumerate(eff_list):
     for r2 in r2_vec:
         eff = eff.scale_r2(r2)
-
+        file_tree.mask = eff.mask.dilate(mask_radius)
         with file_tree.loaded(effect_list=[eff]):
             _folder = folder / f'eff{eff_idx}_r2_{r2:.2e}'
             perm_reg = PermuteRegressVBA(feat_sbj, file_tree,
