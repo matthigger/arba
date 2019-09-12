b08dad9d5a584150a042d65f7a3e67ae81b61edb
matt higger <matt.higger@gmail.com>
2019-09-12 13:52:15 -0400

    add / revise - effect.get_r2() - add a contrast parameter, subtracts out nuisance effects before computing r2
    revise - effect.get_r2() - minor variable renaming for clarity

--- arba/effect/get_r2.py
+++ arba/effect/get_r2.py
@@ -1,19 +1,31 @@
 import numpy as np
 
 
-def get_r2(beta, y, x, y_pool_cov=None):
+def get_r2(beta, y, x, y_pool_cov=None, contrast=None):
     """ computes r2
 
     Args:
-        beta (np.array):
-        y (np.array): (num_sbj, dim_img) imaging features
-        x (np.array): (num_sbj, dim_sbj) subject features
-        y_pool_cov (np.array): pooled (across sbj) of imaging feature cov
-                                 across region
+        beta (np.array): (dim_x, dim_y) linear mapping
+        x (np.array): (num_obs, dim_x) features in the mapping's domain
+        y (np.array): (num_obs, dim_y) features in mapping's range
+        y_pool_cov (np.array): pooled covariance of x (per observation)
+        contrast (np.array): (dim_x) boolean array, describes whether each x
+                             feature is a target variable (True) or nuisance
+                             (False).  all nuisance effects in a given beta are
+                             subtracted before r2 is computed
 
     Returns:
         r2 (float): coefficient of determination
     """
+    if contrast is not None:
+        # subtract effect of nuisance variables from y
+        flag_nuisance = np.logical_not(contrast)
+        y = y - x[:, flag_nuisance] @ beta[flag_nuisance, :]
+
+        # zero nuisance feature (after copying to preserve input x)
+        x = np.array(x)
+        x[:, flag_nuisance] = 0
+
     # get tr_pool_cov
     if y_pool_cov is None:
         tr_pool_cov = 0
@@ -21,15 +33,15 @@ def get_r2(beta, y, x, y_pool_cov=None):
         tr_pool_cov = np.trace(y_pool_cov)
 
     # compute error
-    delta = y - x @ beta
+    error = y - x @ beta
 
-    # compute eps, covariance of error
-    eps = np.atleast_2d((delta.T @ delta) / delta.shape[0])
+    # compute mse
+    mse = np.atleast_2d((error.T @ error) / error.shape[0])
 
     # compute covariance of imaging features
-    cov = np.atleast_2d(np.cov(y.T, ddof=0))
+    y_cov = np.atleast_2d(np.cov(y.T, ddof=0))
 
     # compute r2
-    r2 = 1 - (np.trace(eps) + tr_pool_cov) / (np.trace(cov) + tr_pool_cov)
+    r2 = 1 - (np.trace(mse) + tr_pool_cov) / (np.trace(y_cov) + tr_pool_cov)
 
     return r2
