233e7f980332ca7b627b5f5a83f0be05d3a6ebe5
matt higger <matt.higger@gmail.com>
2019-10-03 15:32:10 -0400

    minor

--- arba/permute/__init__.py
+++ arba/permute/__init__.py
@@ -2,5 +2,5 @@ from .get_perm_matrix import *
 from .get_pval import *
 from .permute_discrim import *
 from .permute_regress import *
-from .permute_regress_vba import *
+from .permute_vba import *
 from .split import *
