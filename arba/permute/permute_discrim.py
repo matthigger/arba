3915a73b030685eacc5ee62d9f37d07fb5334c27
matt higger <matt.higger@gmail.com>
2019-10-03 11:17:59 -0400

    revise - PermuteDiscriminate.permute() - now runs agglomerative clustering per permutation, pval + z score to determine which regions are declared significant

--- arba/permute/permute_discrim.py
+++ arba/permute/permute_discrim.py
@@ -2,8 +2,10 @@ import pathlib
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import interp1d
 from scipy.spatial.distance import dice
 
+from .get_pval import get_pval
 from .permute import Permute
 from ..plot import save_fig
 from ..region import RegionDiscriminate
@@ -28,6 +30,9 @@ class PermuteDiscriminate(Permute):
 
         super().__init__(*args, **kwargs)
 
+        self.node_pval_dict = dict()
+        self.node_z_dict = dict()
+
         # get mask of estimate
         self.node_t2_dict = self.merge_record.fnc_node_val_list['t2']
         self.sig_node = self.get_sig_node()
@@ -47,31 +52,78 @@ class PermuteDiscriminate(Permute):
                                fnc_dict={'t2': get_t2})
 
     def run_single_permute(self, seed):
+        # run agglomerative clustering
         sg_hist = self.get_sg_hist(seed)
-        t2_list = sg_hist.merge_record.fnc_node_val_list['t2'].values()
+        sg_hist.reduce_to(1, verbose=self.verbose)
 
-        return {'t2': sorted(t2_list, reverse=True)}
+        merge_record = sg_hist.merge_record
+        max_size = len(merge_record.leaf_ijk_dict)
+
+        # record max t2 per size
+        node_t2_dict = merge_record.fnc_node_val_list['t2']
+        max_t2 = np.zeros(max_size)
+        for node, t2 in node_t2_dict.items():
+            size = merge_record.node_size_dict[node]
+            if t2 > max_t2[size - 1]:
+                max_t2[size - 1] = t2
+
+        # record which sizes are observed (unobserved will be interpolated)
+        observed = max_t2 != 0
+
+        # ensure monotonicity (increase max_t2 values)
+        for idx in range(len(max_t2) - 2, 0, -1):
+            if max_t2[idx] < max_t2[idx + 1]:
+                max_t2[idx] = max_t2[idx + 1]
+
+        # fill unobserved with linearly interpolated values
+        size = np.array(range(max_size))
+        fnc = interp1d(size[observed], max_t2[observed])
+        max_t2 = fnc(size)
+
+        return {'t2': max_t2}
 
     def permute(self, par_flag=False):
         val_list = super().permute(par_flag=par_flag)
 
         self.t2_null = np.vstack(d['t2'] for d in val_list)
+        self.t2_null = np.sort(self.t2_null, axis=0)
 
         return val_list
 
     def get_sig_node(self):
-
         # get nodes with largest t2
         node_t2_dict = self.merge_record.fnc_node_val_list['t2']
-        sig_node = self.merge_record._cut_greedy(node_t2_dict, max_flag=False)
+        self.node_pval_dict = dict()
+        self.node_z_dict = dict()
+        for n in self.merge_record.nodes:
+            reg_size = self.merge_record.node_size_dict[n]
+            t2 = node_t2_dict[n]
+            stat_null = self.t2_null[:, reg_size - 1]
 
-        # choose only significant nodes
-        thresh = np.percentile(self.t2_null[:, 0], (1 - self.alpha) * 100)
-        sig_node = [n for n in sig_node if node_t2_dict[n] >= thresh]
+            self.node_pval_dict[n] = get_pval(stat=t2,
+                                              stat_null=stat_null,
+                                              sort_flag=False,
+                                              stat_include_flag=True)
+
+            self.node_z_dict[n] = (t2 - np.mean(stat_null)) / np.std(stat_null)
+
+        # keys are nodes, values are tuples of pval and negative z score. these
+        # tuples sort the nodes from most compelling (smallest value) to least
+        node_p_negz_dict = dict()
+        for n, p in self.node_pval_dict.items():
+            if p > self.alpha:
+                # node isn't significant
+                continue
+            node_p_negz_dict[n] = p, -self.node_z_dict[n]
+
+        # cut to a disjoint set of the most compelling nodes
+        sig_node = self.merge_record._cut_greedy(node_p_negz_dict,
+                                                 max_flag=False)
 
         return sig_node
 
-    def save(self, size_v_t2=True, size_v_t2_null=True):
+    def save(self, size_v_t2=True, size_v_t2_null=True, size_v_t2_pval=True,
+             size_v_t2_z=True):
 
         self.folder = pathlib.Path(self.folder)
         self.folder.mkdir(exist_ok=True, parents=True)
@@ -84,6 +136,18 @@ class PermuteDiscriminate(Permute):
 
         for label, mask in self.mode_est_mask_dict.items():
             mask.to_nii(self.folder / f'mask_est_{label}.nii.gz')
+
+        if size_v_t2_pval:
+            self.merge_record.plot_size_v(self.node_pval_dict, label='pval',
+                                          mask=self.mask_target,
+                                          log_y=False)
+            save_fig(self.folder / 'size_v_pval.pdf')
+
+        if size_v_t2_z:
+            self.merge_record.plot_size_v(self.node_z_dict, label='r2z',
+                                          mask=self.mask_target,
+                                          log_y=False)
+            save_fig(self.folder / 'size_v_r2z_score.pdf')
 
         if size_v_t2_null:
             # print percentiles of t2 per size
